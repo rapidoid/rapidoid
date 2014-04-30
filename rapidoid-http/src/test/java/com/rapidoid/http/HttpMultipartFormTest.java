25eecd2eef54aa2e2e6bde2f80b026f69a048bff
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-04-30 23:09:15 +0200

    Initial commit: HttpMultipartFormTest

---
+++ rapidoid-http/src/test/java/com/rapidoid/http/HttpMultipartFormTest.java
@@ -0,0 +1,42 @@
+package com.rapidoid.http;
+
+/*
+ * #%L
+ * rapidoid-http
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.util.U;
+import org.testng.annotations.Test;
+
+public class HttpMultipartFormTest extends HttpTestCommons {
+
+	@Test
+	public void shouldHandleUploads() throws Throwable {
+		server();
+		eq(upload("/", U.map("a", "bb"), U.map("f1", "test1.txt", "f2", "test2.txt")), "POST:/:/:");
+		shutdown();
+	}
+
+	@Test
+	public void shouldHandleBigUploads() throws Throwable {
+		server();
+		eq(upload("/", U.map("a", "bb"), U.map("f1", "test1.txt", "f2", "test2.txt", "f3", "rabbit.jpg")), "POST:/:/:");
+		shutdown();
+	}
+
+}
