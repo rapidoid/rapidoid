4603cf184274b51b31ce73448d83b35232cba777
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-04-30 23:08:48 +0200

    Initial commit: StatsMeasure

---
+++ rapidoid-measure/src/main/java/com/rapidoid/measure/StatsMeasure.java
@@ -0,0 +1,56 @@
+package com.rapidoid.measure;
+
+/*
+ * #%L
+ * rapidoid-measure
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+public class StatsMeasure implements Measure {
+
+	private long min = Long.MAX_VALUE;
+	private long max = Long.MIN_VALUE;
+	private long sum = 0;
+	private long count = 0;
+
+	@Override
+	public String get() {
+		return count > 0 ? String.format("[%s..%s..%s]/%s", min, sum / count, max, count) : null;
+	}
+
+	@Override
+	public void reset() {
+		min = Long.MAX_VALUE;
+		max = Long.MIN_VALUE;
+		sum = 0;
+		count = 0;
+	}
+
+	public synchronized void add(long value) {
+		if (min > value) {
+			min = value;
+		}
+
+		if (max < value) {
+			max = value;
+		}
+
+		count++;
+		sum += value;
+	}
+
+}
