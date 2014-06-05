a4a604891fb8a2c8875483b02aa6a563630a3dee
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-06-05 16:08:14 +0200

    Introduced test skeleton for U.

---
+++ rapidoid-u/src/test/java/org/rapidoid/util/UTest.java
@@ -0,0 +1,758 @@
+package org.rapidoid.util;
+
+/*
+ * #%L
+ * rapidoid-u
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.test.TestCommons;
+import org.testng.annotations.Test;
+
+public class UTest extends TestCommons {
+
+	@Test
+	public void testPropertiesOf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKindOfClassOfQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKindOfObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTraceString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTraceStringStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTraceStringStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTraceStringStringObjectStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDebugString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDebugStringStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDebugStringStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDebugStringStringObjectStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInfoString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInfoStringStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInfoStringStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInfoStringStringObjectStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWarnString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWarnStringStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWarnStringStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWarnStringStringObjectStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWarnStringThrowable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testErrorString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testErrorStringStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testErrorStringStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testErrorStringStringObjectStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testErrorStringThrowable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSevereString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSevereStringStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSevereStringStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSevereStringStringObjectStringObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSevereStringThrowable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSleep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitInterruption() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitForObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFieldValueObjectStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFieldValueFieldObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFieldValueObjectString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFieldValueFieldObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFields() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFieldsAnnotated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInvokeStatic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInvoke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImplementedInterfaces() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConstructor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnnotatedMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTextCollectionOfObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTextObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTextObjectArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTextIteratorOfQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTextln() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReplaceText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoinObjectArrayString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoinIterableOfQString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderObjectArrayStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderIterableOfQStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testXor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEq() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFailIf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExpandTArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExpandTArrayT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubarray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMapKV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMapKVKV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMapKVKVKV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMapKVKVKVKV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAutoExpandingMapF1OfVK() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAutoExpandingMapClassOfV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitForAtomicBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitForAtomicIntegerInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerializeObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserializeByteArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerializeObjectByteBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserializeByteBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExpandByteBufferInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExpandByteBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuf2str() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCopyNtimes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRteStringThrowable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRteThrowable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRteStringObjectArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRteStringThrowableObjectArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnsureBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnsureBooleanString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnsureBooleanStringLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnsureBooleanStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnsureBooleanStringObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotNullObjectArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotNullObjectString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotReady() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotSupported() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotExpected() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStackTraceOf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBenchmark() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCpuMemStats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateProxy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImplementInterfacesObjectInvocationHandlerClassOfQArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImplementInterfacesObjectInvocationHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTracer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRteClassOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRteClassOfTString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewInstanceClassOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewInstanceClassOfTObjectArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitNewInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSchedule() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartMeasure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEndMeasure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSingleton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArgs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasOption() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOptionStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOptionStringLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOptionStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListenIntF2OfVoidBufferedReaderDataOutputStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListenStringIntF2OfVoidBufferedReaderDataOutputStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMicroHttpServer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReplace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testManage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInstantiate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAutowire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWireT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWireTObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWireClassOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWireClassOfTObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvert() {
+		fail("Not yet implemented");
+	}
+
+}
