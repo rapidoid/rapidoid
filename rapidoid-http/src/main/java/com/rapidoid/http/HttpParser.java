bc2c5bf70e8078693aceef69da5ed584e552a40f
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-09-18 18:20:27 +0200

    Refactored HTTP parser and completed multi-part data parsing.

--- rapidoid-http/src/main/java/com/rapidoid/http/HttpParser.java
+++ rapidoid-http/src/main/java/com/rapidoid/http/HttpParser.java
@@ -42,6 +42,34 @@ public class HttpParser implements Constants {
 
 	private static final byte[] CONTENT_LENGTH = "Content-Length:".getBytes();
 
+	private static final byte[] COOKIE = "Cookie".getBytes();
+
+	private static final byte[] MULTIPART_FORM_DATA_BOUNDARY1 = "multipart/form-data; boundary=".getBytes();
+
+	private static final byte[] MULTIPART_FORM_DATA_BOUNDARY2 = "multipart/form-data;boundary=".getBytes();
+
+	private static final byte[] CONTENT_TYPE = "Content-Type".getBytes();
+
+	private static final byte[] CONTENT_DISPOSITION = "Content-Disposition".getBytes();
+
+	private static final byte[] FORM_DATA = "form-data;".getBytes();
+
+	private static final byte[] NAME_EQ = "name=".getBytes();
+
+	private static final byte[] FILENAME_EQ = "filename=".getBytes();
+
+	private static final byte[] CHARSET_EQ = "charset=".getBytes();
+
+	private static final byte[] _UTF_8 = "UTF-8".getBytes();
+
+	private static final byte[] CONTENT_TRANSFER_ENCODING = "Content-Transfer-Encoding".getBytes();
+
+	private static final byte[] _7BIT = "7bit".getBytes();
+
+	private static final byte[] _8BIT = "8bit".getBytes();
+
+	private static final byte[] BINARY = "binary".getBytes();
+
 	public void parse(Buf buf, Bool isGet, Bool isKeepAlive, Range body, Range verb, Range uri, Range path,
 			Range query, Range protocol, Ranges headers, RapidoidHelper helper) {
 
@@ -67,7 +95,7 @@ public class HttpParser implements Constants {
 
 		isKeepAlive.value = isKeepAlive(buf, headers, connPos);
 
-		parsePathAndQuery(buf, uri, path, query);
+		buf.split(uri, ASTERISK, path, query, false);
 
 		if (!getReq) {
 			parseBody(buf, body, headers, helper);
@@ -137,46 +165,216 @@ public class HttpParser implements Constants {
 		buf.limit(limit);
 	}
 
-	public void parsePathAndQuery(Buf buf, Range uri, Range path, Range query) {
-		if (buf.split(uri, ASTERISK, path, query)) {
-		} else {
-			path.assign(uri);
-			query.reset();
-		}
-	}
-
-	public void parseHeaders(Buf buf, int from, int to, KeyValueRanges hdr) {
+	public int parseHeaders(Buf buf, int from, int to, KeyValueRanges headersKV, RapidoidHelper helper) {
 		int pos = buf.position();
 		int limit = buf.limit();
 
 		buf.position(from);
 		buf.limit(to);
 
-		int ind;
-		do {
-			ind = hdr.add();
-			buf.scanUntil(CR, hdr.keys[ind], false);
-			buf.position(buf.position() + 1);
-		} while (!hdr.keys[ind].isEmpty());
+		Int result = helper.integers[0];
+
+		Ranges headers = helper.ranges2;
+		buf.scanLnLn(headers, 0, result);
+
+		parseHeadersIntoKV(buf, headers, headersKV, null);
+
+		int bodyPos = buf.position();
 
 		buf.position(pos);
 		buf.limit(limit);
+
+		return bodyPos;
 	}
 
-	public void parseHeadersIntoKV(Buf buf, Ranges headers, KeyValueRanges headersKV) {
+	public void parseHeadersIntoKV(Buf buf, Ranges headers, KeyValueRanges headersKV, KeyValueRanges cookies) {
 		for (int i = 0; i < headers.count; i++) {
 			Range hdr = headers.ranges[i];
 			int ind = headersKV.add();
-			Range keys = headersKV.keys[ind];
-			Range vals = headersKV.values[ind];
+			Range key = headersKV.keys[ind];
+			Range val = headersKV.values[ind];
 
-			buf.split(hdr, COL, keys, vals);
-			buf.trim(keys);
-			buf.trim(vals);
-			if (keys.isEmpty() || vals.isEmpty()) {
-				headersKV.count--;
+			boolean split = buf.split(hdr, COL, key, val, true);
+			U.ensure(split, "Invalid HTTP header!");
+
+			if (buf.matches(key, COOKIE, false)) {
+				headersKV.count--; // don't include cookies in headers
+
+				int cind = cookies.add();
+				buf.split(val, EQ, cookies.keys[cind], cookies.values[cind], true);
 			}
 		}
+	}
+
+	public void parseBody(Buf src, KeyValueRanges headers, Range body, KeyValueRanges data, KeyValueRanges files,
+			RapidoidHelper helper) {
+
+		Range multipartBoundary = helper.ranges5.ranges[0];
+
+		if (isMultipartForm(src, headers, multipartBoundary)) {
+			helper.bytes[0] = '-';
+			helper.bytes[1] = '-';
+
+			src.get(multipartBoundary, helper.bytes, 2);
+
+			U.failIf(multipartBoundary.isEmpty(), "Invalid multi-part HTTP request!");
+
+			parseMultiParts(src, body, data, files, multipartBoundary, helper);
+		}
+
+	}
+
+	/* http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.2 */
+	private void parseMultiParts(Buf src, Range body, KeyValueRanges data, KeyValueRanges files,
+			Range multipartBoundary, RapidoidHelper helper) {
+
+		int start = body.start;
+		int limit = body.limit();
+
+		int sepLen = multipartBoundary.length + 2;
+		int pos1 = -1, pos2;
+
+		try {
+
+			while ((pos2 = src.find(start, limit, helper.bytes, 0, sepLen, true)) >= 0) {
+				// U.print("****** PARSE MULTI *** pos2=" + pos2);
+
+				if (pos1 >= 0 && pos2 >= 0) {
+					int from = pos1 + sepLen + 2;
+					int to = pos2 - 2;
+					parseMultiPart(src, body, data, files, multipartBoundary, helper, from, to);
+				}
+
+				pos1 = pos2;
+				start = pos2 + sepLen;
+			}
+
+		} catch (Throwable e) {
+			U.warn("Multipart parse error!", e);
+			throw U.rte("Multipart data parse error!");
+		}
+	}
+
+	private void parseMultiPart(Buf src, Range body, KeyValueRanges data, KeyValueRanges files,
+			Range multipartBoundary, RapidoidHelper helper, int from, int to) {
+
+		KeyValueRanges headers = helper.pairs;
+		Range partBody = helper.ranges4.ranges[0];
+		Range contType = helper.ranges4.ranges[1];
+		Range contEnc = helper.ranges4.ranges[2];
+		Range dispo1 = helper.ranges4.ranges[3];
+		Range dispo2 = helper.ranges4.ranges[4];
+		Range name = helper.ranges4.ranges[5];
+		Range filename = helper.ranges4.ranges[6];
+		Range charset = helper.ranges4.ranges[7];
+
+		headers.reset();
+
+		int bodyPos = parseHeaders(src, from, to, headers, helper);
+		partBody.setInterval(bodyPos, to);
+
+		// form-data; name="a" | form-data; name="f2"; filename="test2.txt"
+		Range disposition = headers.get(src, CONTENT_DISPOSITION, false);
+
+		if (src.startsWith(disposition, FORM_DATA, false)) {
+			disposition.strip(FORM_DATA.length, 0);
+		} else {
+			return;
+		}
+
+		src.split(disposition, SEMI_COL, dispo1, dispo2, true);
+
+		if (!parseDisposition(src, dispo1, dispo2, name, filename)) {
+			if (!parseDisposition(src, dispo2, dispo1, name, filename)) {
+				throw U.rte("Unrecognized Content-disposition header!");
+			}
+		}
+
+		// (OPTIONAL) e.g. application/octet-stream | text/plain;
+		// charset=ISO-8859-1 | image/svg+xml | text/plain; charset=utf-8 |
+		// | multipart/mixed; boundary=BbC04y | application/pdf |
+		// application/vnd.oasis.opendocument.text | image/gif |
+		// video/mp4; codecs="avc1.640028 | DEFAULT=text/plain
+		Range contentType = headers.get(src, CONTENT_TYPE, false);
+
+		charset.reset();
+		contType.reset();
+		contEnc.reset();
+
+		if (contentType != null) {
+			src.split(contentType, SEMI_COL, contType, contEnc, true);
+			if (src.startsWith(contEnc, CHARSET_EQ, false)) {
+				charset.assign(contEnc);
+				charset.strip(CHARSET_EQ.length, 0);
+				src.trim(charset);
+
+				U.failIf(!src.matches(charset, _UTF_8, false), "Only the UTF-8 charset is supported!");
+			}
+		}
+
+		// (OPTIONAL) e.g. 7bit | 8bit | binary | DEFAULT=7bit
+		Range encoding = headers.get(src, CONTENT_TRANSFER_ENCODING, false);
+
+		if (encoding != null) {
+			boolean validEncoding = src.matches(encoding, _7BIT, false) || src.matches(encoding, _8BIT, false)
+					|| src.matches(encoding, BINARY, false);
+			U.failIf(!validEncoding, "Invalid Content-transfer-encoding header value!");
+		}
+
+		if (filename.isEmpty()) {
+			int ind = data.add();
+			data.keys[ind].assign(name);
+			data.values[ind].assign(partBody);
+		} else {
+			int ind = files.add();
+			files.keys[ind].assign(name);
+			files.values[ind].assign(partBody);
+		}
+	}
+
+	private boolean parseDisposition(Buf src, Range dispoA, Range dispoB, Range name, Range filename) {
+		if (src.startsWith(dispoA, NAME_EQ, false)) {
+
+			name.assign(dispoA);
+			name.strip(NAME_EQ.length, 0);
+			src.trim(name);
+			name.strip(1, 1);
+
+			if (src.startsWith(dispoB, FILENAME_EQ, false)) {
+				filename.assign(dispoB);
+				filename.strip(FILENAME_EQ.length, 0);
+				src.trim(filename);
+				filename.strip(1, 1);
+			} else {
+				filename.reset();
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isMultipartForm(Buf buf, KeyValueRanges headers, Range multipartBoundary) {
+		Range contType = headers.get(buf, CONTENT_TYPE, false);
+
+		if (contType != null) {
+			// TODO improve parsing of "multipart" and "data boundary"
+
+			if (buf.startsWith(contType, MULTIPART_FORM_DATA_BOUNDARY1, false)) {
+				multipartBoundary.setInterval(contType.start + MULTIPART_FORM_DATA_BOUNDARY1.length, contType.limit());
+				return true;
+			}
+
+			if (buf.startsWith(contType, MULTIPART_FORM_DATA_BOUNDARY2, false)) {
+				multipartBoundary.setInterval(contType.start + MULTIPART_FORM_DATA_BOUNDARY2.length, contType.limit());
+				return true;
+			}
+		}
+
+		multipartBoundary.reset();
+
+		return false;
 	}
 
 }
--- rapidoid-http/src/main/java/com/rapidoid/http/HttpProtocol.java
+++ rapidoid-http/src/main/java/com/rapidoid/http/HttpProtocol.java
@@ -21,20 +21,10 @@ package com.rapidoid.http;
  */
 
 import org.rapidoid.Ctx;
-import org.rapidoid.buffer.Buf;
-import org.rapidoid.data.KeyValueRanges;
-import org.rapidoid.data.Range;
 import org.rapidoid.net.ExchangeProtocol;
-import org.rapidoid.net.RapidoidHelper;
 import org.rapidoid.util.U;
 
 public class HttpProtocol extends ExchangeProtocol<WebExchangeImpl> {
-
-	private static final byte[] CONTENT_TYPE = "Content-Type".getBytes();
-
-	private static final byte[] MULTIPART_FORM_DATA_BOUNDARY1 = "multipart/form-data; boundary=".getBytes();
-
-	private static final byte[] MULTIPART_FORM_DATA_BOUNDARY2 = "multipart/form-data;boundary=".getBytes();
 
 	private final HttpParser parser = U.inject(HttpParser.class);
 
@@ -49,132 +39,44 @@ public class HttpProtocol extends ExchangeProtocol<WebExchangeImpl> {
 	}
 
 	@Override
-	protected void process(Ctx ctx, WebExchangeImpl web) {
+	protected void process(Ctx ctx, WebExchangeImpl xch) {
 
-		parser.parse(web.input(), web.isGet, web.isKeepAlive, web.body, web.verb, web.uri, web.path, web.query,
-				web.protocol, web.headers, web.helper());
+		parser.parse(xch.input(), xch.isGet, xch.isKeepAlive, xch.body, xch.verb, xch.uri, xch.path, xch.query,
+				xch.protocol, xch.headers, xch.helper());
 
 		U.failIf(xch.verb.isEmpty() || xch.uri.isEmpty(), "Invalid HTTP request!");
 		U.failIf(xch.isGet.value && !xch.body.isEmpty(), "Body is NOT allowed in HTTP GET requests!");
 
 		xch.respType = HttpResponses.TEXT_HTML;
 
-		int posBefore = web.output().size();
+		int posBefore = xch.output().size();
 
-		web.output().append(resp(web).bytes());
+		xch.output().append(resp(xch).bytes());
 
 		try {
-			boolean dispatched = router.dispatch(web);
+			boolean dispatched = router.dispatch(xch);
 			if (!dispatched) {
-				web.write("Invalid HTTP VERB or URL PATH!");
-				web.done();
+				xch.write("Invalid HTTP VERB or URL PATH!");
+				xch.done();
 			}
 		} catch (Throwable e) {
-			U.error("Internal server error!", "request", web, "error", e);
-			web.write("Internal server error!");
-			web.done();
+			U.error("Internal server error!", "request", xch, "error", e);
+			xch.write("Internal server error!");
+			xch.done();
 		}
 
-		long wrote = web.getTotalWritten();
+		long wrote = xch.getTotalWritten();
 		U.ensure(wrote <= Integer.MAX_VALUE, "Response too big!");
 
-		int pos = posBefore + resp(web).contentLengthPos;
+		int pos = posBefore + resp(xch).contentLengthPos;
 
-		web.output().putNumAsText(pos, wrote);
+		xch.output().putNumAsText(pos, wrote);
 	}
 
-	private HttpResponse resp(WebExchangeImpl exchange) {
-		HttpResponse resp = responses.get(exchange.isKeepAlive.value, exchange.respType);
+	private HttpResponse resp(WebExchangeImpl xch) {
+		HttpResponse resp = responses.get(xch.isKeepAlive.value, xch.respType);
 		assert resp != null;
 		return resp;
-	}
-
-	private void analyzeRequest(WebExchangeImpl exchange) {
-		Buf src = exchange.input();
-
-		RapidoidHelper helper = exchange.helper();
-
-		if (isMultipartForm(exchange)) {
-			helper.bytes[0] = '-';
-			helper.bytes[1] = '-';
-
-			src.get(exchange.multipartBoundary, helper.bytes, 2);
-
-			// U.show(src.get(exchange.multipartBoundary));
-			// U.show(new String(helper.bytes, 0,
-			// exchange.multipartBoundary.length + 2));
-			// U.print("# ********************");
-			// U.show(src.get(exchange.body));
-			// U.print("# ********************");
-
-			parseMultiParts(exchange);
-		}
-	}
-
-	/* http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.2 */
-	private void parseMultiParts(WebExchangeImpl exchange) {
-		Buf src = exchange.input();
-
-		int start = exchange.body.start;
-		int limit = exchange.body.limit();
-
-		int sepLen = exchange.multipartBoundary.length + 2;
-		int pos1 = -1, pos2;
-
-		U.print("****** PARSING MULTI  **************");
-		try {
-			while ((pos2 = src.find(start, limit, exchange.helper().bytes, 0, sepLen, true)) >= 0) {
-				U.print("****** PARSE MULTI *** pos2=" + pos2);
-
-				if (pos1 >= 0 && pos2 >= 0) {
-					int from = pos1 + sepLen + 2;
-					int to = pos2 - 2;
-					// U.show(src.get(Range.fromTo(from, to)));
-
-					KeyValueRanges headers = exchange.helper().pairs;
-					headers.reset();
-
-					int aa = src.limit();
-
-					parser.parseHeaders(src, from, to, headers);
-
-					src.limit(aa);
-
-					// ?? int bodyStart = src.position(); ??
-					// U.show(src.get(Range.fromTo(bodyStart, to)));
-				}
-
-				pos1 = pos2;
-				start = pos2 + sepLen;
-			}
-
-		} catch (Throwable e) {
-			exchange.fail("Multipart data parse error!");
-			U.warn("Multipart parse error!", e);
-		}
-	}
-
-	private boolean isMultipartForm(WebExchangeImpl exchange) {
-		Buf buf = exchange.input();
-		Range contType = exchange.headers_().ranges().get(buf, CONTENT_TYPE, false);
-
-		if (contType != null) {
-			if (buf.startsWith(contType, MULTIPART_FORM_DATA_BOUNDARY1, false)) {
-				exchange.multipartBoundary.setInterval(contType.start + MULTIPART_FORM_DATA_BOUNDARY1.length,
-						contType.limit());
-				return true;
-			}
-
-			if (buf.startsWith(contType, MULTIPART_FORM_DATA_BOUNDARY2, false)) {
-				exchange.multipartBoundary.setInterval(contType.start + MULTIPART_FORM_DATA_BOUNDARY2.length,
-						contType.limit());
-				return true;
-			}
-		}
-
-		exchange.multipartBoundary.reset();
-
-		return false;
 	}
 
 }
