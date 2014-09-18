ef78c7c6a7a9fa07c2e8ed041705c60e3de001ec
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-09-18 18:04:56 +0200

    Added bytes-to-textual-representation utility in U.

--- rapidoid-u/src/main/java/org/rapidoid/util/U.java
+++ rapidoid-u/src/main/java/org/rapidoid/util/U.java
@@ -2297,4 +2297,14 @@ public class U implements Constants {
 		return String.format(s, args);
 	}
 
+	public static String bytesToString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < bytes.length; i++) {
+			sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		return sb.toString();
+	}
+
 }
