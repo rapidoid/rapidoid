a305a1ce4792cbe4785fea3bc318f1ca6c0d8aa7
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-11-01 18:21:19 +0100

    Added table widget with test.

---
+++ rapidoid-pages/src/main/java/org/rapidoid/pages/widget/TableWidget.java
@@ -0,0 +1,66 @@
+package org.rapidoid.pages.widget;
+
+import java.util.List;
+
+import org.rapidoid.model.Item;
+import org.rapidoid.model.Items;
+import org.rapidoid.model.Property;
+import org.rapidoid.pages.html.TbodyTag;
+import org.rapidoid.pages.html.TrTag;
+
+/*
+ * #%L
+ * rapidoid-pages
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+public class TableWidget extends Widget {
+
+	private final List<Property> properties;
+
+	private final Items items;
+
+	public TableWidget(Items items) {
+		this.items = items;
+		this.properties = items.properties();
+	}
+
+	@Override
+	protected Object contents() {
+		TrTag header = tr();
+
+		for (Property prop : properties) {
+			header.append(th(prop.caption()));
+		}
+
+		TbodyTag body = tbody();
+
+		for (int i = 0; i < items.size(); i++) {
+			TrTag row = tr();
+			Item item = items.get(i);
+
+			for (Property prop : properties) {
+				row.append(td(item.get(prop.name())));
+			}
+
+			body.append(row);
+		}
+
+		return table(thead(header), body);
+	}
+
+}
---
+++ rapidoid-pages/src/test/java/org/rapidoid/pages/widget/TableWidgetTest.java
@@ -0,0 +1,48 @@
+package org.rapidoid.pages.widget;
+
+/*
+ * #%L
+ * rapidoid-pages
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.model.Items;
+import org.rapidoid.model.Model;
+import org.rapidoid.pages.Person;
+import org.rapidoid.util.U;
+import org.testng.annotations.Test;
+
+public class TableWidgetTest extends WidgetTestCommons {
+
+	@Test
+	public void testPagerButtons() {
+		Items items = Model.beanItems(new Person("John", 20), new Person("Rambo", 50));
+
+		TableWidget table = new TableWidget(items);
+		U.print(table);
+
+		has(table, "<th>name</th>");
+		has(table, "<th>age</th>");
+
+		has(table, "<td>John</td>");
+		has(table, "<td>20</td>");
+
+		has(table, "<td>Rambo</td>");
+		has(table, "<td>50</td>");
+	}
+
+}
