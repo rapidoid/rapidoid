de7e83dcc4f0382d38fad5cf688246975420cf59
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-11-01 13:59:23 +0100

    Introduced basic model API.

---
+++ rapidoid-model/src/main/java/org/rapidoid/model/Items.java
@@ -0,0 +1,49 @@
+package org.rapidoid.model;
+
+/*
+ * #%L
+ * rapidoid-model
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.List;
+
+public interface Items {
+
+	void insert(int index, Item item);
+
+	void add(Item item);
+
+	void addAll(Items items);
+
+	void clear();
+
+	Item get(int index);
+
+	boolean isEmpty();
+
+	void remove(int index);
+
+	void set(int index, Item item);
+
+	int size();
+
+	Items range(int fromIndex, int toIndex);
+
+	List<Property> properties();
+
+}
