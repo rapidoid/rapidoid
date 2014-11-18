38fd0d19e4550eb859d56753c1f75d5afd065a9e
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-11-19 01:39:51 +0200

    Cleaned up the "New task" screen.

--- rapidoid-app/src/test/java/org/rapidoid/app/example/NewTaskScreen.java
+++ rapidoid-app/src/test/java/org/rapidoid/app/example/NewTaskScreen.java
@@ -23,7 +23,6 @@ package org.rapidoid.app.example;
 import org.rapidoid.annotation.Session;
 import org.rapidoid.app.GUI;
 import org.rapidoid.html.Tag;
-import org.rapidoid.html.tag.DivTag;
 import org.rapidoid.html.tag.FormTag;
 import org.rapidoid.html.tag.H1Tag;
 import org.rapidoid.model.Item;
@@ -32,26 +31,18 @@ import org.rapidoid.model.Model;
 public class NewTaskScreen extends GUI {
 
 	@Session
-	private User user = new User();
-
-	@Session
-	private Task task = new Task("aa", Priority.MEDIUM, user);
+	private Task task = new Task("aa", Priority.MEDIUM);
 
 	public Object content() {
 
 		H1Tag caption = h1("Create a new task");
-
-		DivTag caption2 = rowFull(h4("Edit user:"));
 
 		Tag<?>[] buttons = { cmd("Save"), cmd("Cancel") };
 
 		Item item = Model.item(task);
 		FormTag frm1 = form_(item, buttons);
 
-		Item item2 = Model.item(user);
-		FormTag frm2 = form_(item2, buttons);
-
-		return row(col2(), col8(caption, frm1, caption2, frm2), col2());
+		return row(col2(), col8(caption, frm1), col2());
 	}
 
 }
