f8f9cf08c0e2e376fcc3662c9eed7b5ebddaf786
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-12-03 15:40:11 +0100

    Refactored Rapidoid CSS and JS out into separate files.

---
+++ rapidoid-html/src/main/resources/public/rapidoid.js
@@ -0,0 +1,61 @@
+var theme;
+try {
+	var m = /\bTHEME=(\w+?)\b/g.exec(document.cookie);
+	theme = m[1];
+} catch (e) {
+}
+if (theme && theme != 'default') {
+	document.write('<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.0/'
+			+ theme + '/bootstrap.min.css" rel="stylesheet">');
+}
+
+function goAt(url) {
+	window.location.href = url;
+}
+
+function _emit(event) {
+
+	var x = document.querySelectorAll("input,textarea");
+
+	var inputs = {};
+
+	for (var i = 0; i < x.length; i++) {
+		var t = $(x[i]);
+		var _h = t.attr('_h');
+
+		if (_h) {
+			var val;
+
+			if (t.prop('type') == 'checkbox' || t.prop('type') == 'radio') {
+				val = t.prop('checked');
+			} else {
+				val = t.val();
+			}
+
+			inputs[_h] = val;
+		}
+	}
+
+	x = document.querySelectorAll("option");
+
+	for (var i = 0; i < x.length; i++) {
+		var t = $(x[i]);
+		var _h = t.attr('_h');
+
+		if (_h) {
+			inputs[_h] = t.prop('selected');
+		}
+	}
+
+	$.post(window.location.href, {
+		event : event,
+		inputs : JSON.stringify(inputs)
+	}).done(function(data, a, b, c) {
+		for ( var sel in data) {
+			$(sel).html(data[sel]);
+		}
+	}).fail(function(data) {
+		alert("Error!");
+		console.log(data);
+	});
+}
