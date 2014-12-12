c7d5ebe1729b2d286cc62c92f4fbca9a2d625d23
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-12-12 02:24:39 +0100

    Added base class for persisted entities.

---
+++ rapidoid-db/src/main/java/org/rapidoid/db/Entity.java
@@ -0,0 +1,59 @@
+package org.rapidoid.db;
+
+/*
+ * #%L
+ * rapidoid-db
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+/**
+ * Base class for persisted domain model entities.
+ */
+public abstract class Entity {
+
+	public long id;
+
+	public long version;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entity other = (Entity) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + "#" + id;
+	}
+
+}
