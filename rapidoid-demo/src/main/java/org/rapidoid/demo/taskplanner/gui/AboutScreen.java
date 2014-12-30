42fb072eef8692b6c78d186b6a985ed9a4487a2f
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-12-30 05:58:41 +0100

    Added 2 modal dialog examples in the "About" screen in Task Planner demo app.

--- rapidoid-demo/src/main/java/org/rapidoid/demo/taskplanner/gui/AboutScreen.java
+++ rapidoid-demo/src/main/java/org/rapidoid/demo/taskplanner/gui/AboutScreen.java
@@ -23,17 +23,32 @@ package org.rapidoid.demo.taskplanner.gui;
 import org.rapidoid.app.Screen;
 import org.rapidoid.db.DB;
 import org.rapidoid.demo.taskplanner.model.Task;
+import org.rapidoid.demo.taskplanner.model.User;
 import org.rapidoid.html.Tag;
 import org.rapidoid.html.tag.ButtonTag;
 import org.rapidoid.util.U;
+
+class Book {
+	public String title;
+	public User author;
+}
 
 public class AboutScreen extends Screen {
 
 	public Object content() {
 		Tag mix = h2(a("Enter the mix").href("/mix"));
 		ButtonTag tx = btn("Transactional").cmd("tx");
+		ButtonTag dlg = cmd("Dialog");
 
-		return arr(mix, tx);
+		return arr(mix, tx, dlg, ADD);
+	}
+
+	public Tag addBook() {
+		return modal("Add new book", edit(new Book()), div(SAVE, CANCEL));
+	}
+
+	public Tag yesNo() {
+		return modal("Confirm deletion", h1("Are you sure?"), div(YES, NO));
 	}
 
 	public void onTx() {
@@ -48,6 +63,24 @@ public class AboutScreen extends Screen {
 		Task task = new Task();
 		task.title = "DON'T GO TO THE DATABASE!";
 		return task;
+	}
+
+	public void onDialog() {
+		modal = "yesNo";
+	}
+
+	public void onAdd() {
+		modal = "addBook";
+	}
+
+	public void onYes() {
+		U.info("yes");
+		modal = null;
+	}
+
+	public void onNo() {
+		U.info("no");
+		modal = null;
 	}
 
 }
