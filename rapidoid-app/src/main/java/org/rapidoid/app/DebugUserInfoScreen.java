1d360d19fd1440f3a7ce1bf7f5b568f15f0af54e
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-01-03 19:19:29 +0100

    Added debug-only helper screen that displays common user information.

---
+++ rapidoid-app/src/main/java/org/rapidoid/app/DebugUserInfoScreen.java
@@ -0,0 +1,40 @@
+package org.rapidoid.app;
+
+/*
+ * #%L
+ * rapidoid-app
+ * %%
+ * Copyright (C) 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.html.Tag;
+import org.rapidoid.http.HttpExchange;
+import org.rapidoid.security.annotation.DevMode;
+
+@DevMode
+public class DebugUserInfoScreen extends Screen {
+
+	public Object content(HttpExchange x) {
+		Tag caption = titleBox("Debug Mode - User Information");
+		if (x.isLoggedIn()) {
+			Object details = show(x.user(), "name", "username", "email");
+			return row(caption, details);
+		} else {
+			return row(caption, h4("Not logged in!"));
+		}
+	}
+
+}
