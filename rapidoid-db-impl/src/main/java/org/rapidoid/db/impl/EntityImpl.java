1a3a38aff7c4621f096efb32b23a4cf5d95b2f4b
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-01-16 01:53:04 +0100

    Implemented hashCode and equals for interface-based entities.

--- rapidoid-db-impl/src/main/java/org/rapidoid/db/impl/EntityImpl.java
+++ rapidoid-db-impl/src/main/java/org/rapidoid/db/impl/EntityImpl.java
@@ -140,4 +140,26 @@ public class EntityImpl implements Entity, EntityInternals {
 		return rel;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id() ^ (id() >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entity other = (Entity) obj;
+		if (id() != other.id())
+			return false;
+		return true;
+	}
+
 }
