45f1bd186904d738ac236d021cd1e922699a72bd
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-01-18 12:04:21 +0100

    Removed Entity interface constraint for the entity type in DB#create.

--- rapidoid-db/src/main/java/org/rapidoid/db/DB.java
+++ rapidoid-db/src/main/java/org/rapidoid/db/DB.java
@@ -205,35 +205,34 @@ public class DB {
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <E extends Entity> E create(Class<E> entityType) {
+	public static <E> E create(Class<E> entityType) {
 		return (E) schema().create(entityType, Collections.EMPTY_MAP);
 	}
 
-	public static <E extends Entity> E create(Class<E> entityType, Map<String, ?> properties) {
+	public static <E> E create(Class<E> entityType, Map<String, ?> properties) {
 		return schema().create(entityType, properties);
 	}
 
-	public static <E extends Entity> E create(Class<E> entityType, String prop, Object value) {
+	public static <E> E create(Class<E> entityType, String prop, Object value) {
 		return schema().create(entityType, U.map(prop, value));
 	}
 
-	public static <E extends Entity> E create(Class<E> entityType, String prop1, Object value1, String prop2,
-			Object value2) {
+	public static <E> E create(Class<E> entityType, String prop1, Object value1, String prop2, Object value2) {
 		return schema().create(entityType, U.map(prop1, value1, prop2, value2));
 	}
 
-	public static <E extends Entity> E create(Class<E> entityType, String prop1, Object value1, String prop2,
-			Object value2, String prop3, Object value3) {
+	public static <E> E create(Class<E> entityType, String prop1, Object value1, String prop2, Object value2,
+			String prop3, Object value3) {
 		return schema().create(entityType, U.map(prop1, value1, prop2, value2, prop3, value3));
 	}
 
-	public static <E extends Entity> E create(Class<E> entityType, String prop1, Object value1, String prop2,
-			Object value2, String prop3, Object value3, String prop4, Object value4) {
+	public static <E> E create(Class<E> entityType, String prop1, Object value1, String prop2, Object value2,
+			String prop3, Object value3, String prop4, Object value4) {
 		return schema().create(entityType, U.map(prop1, value1, prop2, value2, prop3, value3, prop4, value4));
 	}
 
-	public static <E extends Entity> E create(Class<E> entityType, String prop1, Object value1, String prop2,
-			Object value2, String prop3, Object value3, String prop4, Object value4, String prop5, Object value5) {
+	public static <E> E create(Class<E> entityType, String prop1, Object value1, String prop2, Object value2,
+			String prop3, Object value3, String prop4, Object value4, String prop5, Object value5) {
 		return schema().create(entityType,
 				U.map(prop1, value1, prop2, value2, prop3, value3, prop4, value4, prop5, value5));
 	}
