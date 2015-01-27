27ab9ddfe9c26f63887a6f5a67a34be564301ab2
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-01-27 19:34:42 +0100

    Added template rendering example in the Issue Management example app.

--- examples/example-issues/src/main/java/issues/HomeScreen.java
+++ examples/example-issues/src/main/java/issues/HomeScreen.java
@@ -1,7 +1,12 @@
 package issues;
 
 import org.rapidoid.app.Screen;
+import org.rapidoid.db.DB;
 
 public class HomeScreen extends Screen {
+
+	public Object content() {
+		return render("home.html", "count", DB.getAll(Issue.class).size());
+	}
 
 }
