26233d545b9bb347734592c0ced39f82f1e600c9
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-02-17 19:05:56 +0100

    Added 2D and 3D point entities in extras.

---
+++ rapidoid-extras/src/main/java/org/rapidoid/extra/domain/Point3D.java
@@ -0,0 +1,52 @@
+package org.rapidoid.extra.domain;
+
+/*
+ * #%L
+ * rapidoid-extras
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.db.Entity;
+
+@Authors("Nikolche Mihajlovski")
+@Since("2.2.0")
+public class Point3D extends Entity {
+
+	private static final long serialVersionUID = 2482788605164111044L;
+
+	public double x;
+
+	public double y;
+
+	public double z;
+
+	public Point3D() {}
+
+	public Point3D(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + x + ", " + y + ", " + z + "]";
+	}
+
+}
