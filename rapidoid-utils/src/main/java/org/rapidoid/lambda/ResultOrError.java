10f0343c2cc7da606b165a1425951a113cab078c
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-04 01:11:19 +0200

    Added simple async-to-blocking result adapter.

---
+++ rapidoid-utils/src/main/java/org/rapidoid/lambda/ResultOrError.java
@@ -0,0 +1,70 @@
+package org.rapidoid.lambda;
+
+/*
+ * #%L
+ * rapidoid-utils
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.lambda.Callback;
+import org.rapidoid.util.U;
+import org.rapidoid.util.UTILS;
+
+@Authors("Nikolche Mihajlovski")
+@Since("2.5.0")
+public class ResultOrError<T> implements Callback<T> {
+
+	private volatile boolean done;
+
+	private volatile T result;
+
+	private volatile Throwable error;
+
+	public void setResult(T result) {
+		this.result = result;
+		done = true;
+	}
+
+	public void setError(Throwable error) {
+		this.error = error;
+		done = true;
+	}
+
+	@Override
+	public void onDone(T result, Throwable error) {
+		if (error != null) {
+			setError(error);
+		} else {
+			setResult(result);
+		}
+	}
+
+	public T get() {
+		while (!done) {
+			UTILS.sleep(10);
+		}
+
+		if (error != null) {
+			throw U.rte("Cannot get result, there was an error!", error);
+		}
+
+		return result;
+	}
+
+}
