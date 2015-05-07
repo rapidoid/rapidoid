bfb5ea5a10df4f173406a0cb6fd37c9801a6ae16
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 13:04:10 +0200

    Introducing CtxConnection.java

---
+++ rapidoid-x-net/src/main/java/org/rapidoidx/net/abstracts/CtxConnection.java
@@ -0,0 +1,53 @@
+package org.rapidoidx.net.abstracts;
+
+/*
+ * #%L
+ * rapidoid-x-net
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import java.net.InetSocketAddress;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public interface CtxConnection<T> {
+
+	String address();
+
+	InetSocketAddress getAddress();
+
+	long connId();
+
+	boolean isAsync();
+
+	T close();
+
+	T closeIf(boolean condition);
+
+	boolean isClosing();
+
+	boolean isClosed();
+
+	void waitUntilClosing();
+
+	void log(String msg);
+
+}
