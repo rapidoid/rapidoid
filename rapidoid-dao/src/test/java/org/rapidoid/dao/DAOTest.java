e8b628f329af58cb6c623668dde8de9d951768e6
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 16:49:17 +0200

    Added simple test for the generic DAO.

---
+++ rapidoid-dao/src/test/java/org/rapidoid/dao/DAOTest.java
@@ -0,0 +1,52 @@
+package org.rapidoid.dao;
+
+/*
+ * #%L
+ * rapidoid-dao
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.plugins.DB;
+import org.rapidoid.test.TestCommons;
+import org.testng.annotations.Test;
+
+class PersonService extends DAO<Person> {}
+
+/**
+ * @author Nikolche Mihajlovski
+ * @since 3.0.0
+ */
+public class DAOTest extends TestCommons {
+
+	@Test
+	public void testCRUDinDAO() {
+
+		PersonService service = new PersonService();
+
+		// exercise the entity type inference
+		eq(service.getEntityType(), Person.class);
+
+		eq(DB.size(), 0);
+		long id = service.insert(new Person("aa", 123));
+		eq(DB.size(), 1);
+
+		Person p = service.get(id);
+		service.delete(p);
+		eq(DB.size(), 0);
+	}
+
+}
