5c522b23364c966aa2e6d06cfdee7e689723047d
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 13:04:10 +0200

    Introducing LoopStatus.java

---
+++ rapidoid-x-net/src/main/java/org/rapidoidx/net/impl/LoopStatus.java
@@ -0,0 +1,33 @@
+package org.rapidoidx.net.impl;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+/*
+ * #%L
+ * rapidoid-x-net
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public enum LoopStatus {
+
+	INIT, BEFORE_LOOP, LOOP, AFTER_LOOP, STOPPED, FAILED;
+
+}
