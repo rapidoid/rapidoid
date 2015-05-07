1491d2087e5ae4bc90b39c78c8cd62a7b08f2533
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 13:04:09 +0200

    Introducing BufferTestCommons.java

---
+++ rapidoid-x-buffer/src/test/java/org/rapidoidx/buffer/BufferTestCommons.java
@@ -0,0 +1,89 @@
+package org.rapidoidx.buffer;
+
+/*
+ * #%L
+ * rapidoid-x-buffer
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import java.nio.ByteBuffer;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.test.TestCommons;
+import org.rapidoidx.data.KeyValueRanges;
+import org.rapidoidx.data.Range;
+import org.testng.Assert;
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public class BufferTestCommons extends TestCommons {
+
+	protected void eq(String whole, Range range, String expected) {
+		eq(range.get(whole), expected);
+	}
+
+	protected void eqs(String whole, KeyValueRanges ranges, String... keysAndValues) {
+		eq(keysAndValues.length % 2, 0);
+		eq(ranges.count, keysAndValues.length / 2);
+		for (int i = 0; i < ranges.count; i++) {
+			Range key = ranges.keys[i];
+			Range value = ranges.values[i];
+			eq(whole, key, keysAndValues[i * 2]);
+			eq(whole, value, keysAndValues[i * 2 + 1]);
+		}
+	}
+
+	protected void eq(Range range, int start, int length) {
+		Assert.assertEquals(range.start, start);
+		Assert.assertEquals(range.length, length);
+	}
+
+	protected void isNone(Range range) {
+		Assert.assertEquals(range.start, -1);
+		Assert.assertEquals(range.length, 0);
+	}
+
+	protected void eq(Buf buf, String expected) {
+		eq(buf.size(), expected.getBytes().length);
+		eq(buf.data(), expected);
+
+		byte[] bbytes = new byte[buf.size()];
+		ByteBuffer bufy = ByteBuffer.wrap(bbytes);
+		buf.writeTo(bufy);
+		eq(new String(bbytes), expected);
+
+		int size = (int) Math.ceil(expected.length() * 1.0 / buf.unitSize());
+		isTrue(buf.unitCount() == size || buf.unitCount() == size + 1);
+
+		byte[] bytes = expected.getBytes();
+		synchronized (buf) {
+			for (int i = 0; i < bytes.length; i++) {
+				eq((char) buf.get(i), (char) bytes[i]);
+			}
+		}
+
+		for (int len = 2; len < 10; len++) {
+			for (int p = 0; p <= buf.size() - len; p++) {
+				String sub = buf.get(new Range(p, len));
+				eq(sub, expected.substring(p, p + len));
+			}
+		}
+	}
+
+}
