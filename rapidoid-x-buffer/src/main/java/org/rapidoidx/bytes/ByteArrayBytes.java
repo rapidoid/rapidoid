ddbc2e4d9056d2f3cdfd0b927b1b38f83edcd6c3
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 13:04:09 +0200

    Introducing ByteArrayBytes.java

---
+++ rapidoid-x-buffer/src/main/java/org/rapidoidx/bytes/ByteArrayBytes.java
@@ -0,0 +1,47 @@
+package org.rapidoidx.bytes;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+/*
+ * #%L
+ * rapidoid-x-buffer
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public class ByteArrayBytes implements Bytes {
+
+	private final byte[] bytes;
+
+	public ByteArrayBytes(byte[] bytes) {
+		this.bytes = bytes;
+	}
+
+	@Override
+	public byte get(int position) {
+		return bytes[position];
+	}
+
+	@Override
+	public int limit() {
+		return bytes.length;
+	}
+
+}
