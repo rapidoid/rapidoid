e2304eb8774fdaaa2bb25f458bb93d668117eafd
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 13:04:07 +0200

    Introducing IPost.java

---
+++ rapidoid-x-db-tests/src/test/java/org/rapidoidx/db/model/IPost.java
@@ -0,0 +1,44 @@
+package org.rapidoidx.db.model;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Rel;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.entity.IEntity;
+import org.rapidoidx.db.DbColumn;
+import org.rapidoidx.db.DbRef;
+import org.rapidoidx.db.DbSet;
+
+/*
+ * #%L
+ * rapidoid-x-db-tests
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public interface IPost extends IEntity {
+
+	DbColumn<String> content();
+
+	@Rel("^posted")
+	DbRef<IProfile> postedOn();
+
+	@Rel("likes")
+	DbSet<IPerson> likes();
+
+}
