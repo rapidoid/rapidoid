006928c5175b9d4610b81b02bf97aacb2f7efb8d
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-07 13:04:07 +0200

    Introducing DbSchemaImpl.java

---
+++ rapidoid-x-db-impl/src/main/java/org/rapidoidx/db/impl/DbSchemaImpl.java
@@ -0,0 +1,154 @@
+package org.rapidoidx.db.impl;
+
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.DbEntity;
+import org.rapidoid.annotation.Scaffold;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.beany.Beany;
+import org.rapidoid.entity.IEntity;
+import org.rapidoid.util.Cls;
+import org.rapidoid.util.English;
+import org.rapidoid.util.Scan;
+import org.rapidoid.util.U;
+import org.rapidoidx.db.DbDsl;
+import org.rapidoidx.db.DbSchema;
+
+/*
+ * #%L
+ * rapidoid-x-db-impl
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public class DbSchemaImpl implements DbSchema {
+
+	private final ConcurrentMap<String, Class<?>> entityTypes = U.concurrentMap();
+
+	private final ConcurrentMap<String, Class<?>> entityTypesPlural = U.concurrentMap();
+
+	public DbSchemaImpl() {
+		for (Class<?> entityType : Scan.annotated(DbEntity.class)) {
+			putEntityType(entityType);
+		}
+		for (Class<?> entityType : Scan.annotated(Scaffold.class)) {
+			putEntityType(entityType);
+		}
+	}
+
+	@Override
+	public <E> DbDsl<E> dsl(Class<E> entityType) {
+		putEntityType(entityType);
+
+		return null; // FIXME implement this
+	}
+
+	private <E> void putEntityType(Class<E> entityType) {
+		String type = entityType.getSimpleName().toLowerCase();
+
+		entityTypes.putIfAbsent(type, entityType);
+		entityTypesPlural.putIfAbsent(English.plural(type), entityType);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <E> Class<E> getEntityType(String typeName) {
+		return (Class<E>) entityTypes.get(typeName.toLowerCase());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <E> Class<E> getEntityTypeFromPlural(String typeNamePlural) {
+		return (Class<E>) entityTypesPlural.get(typeNamePlural.toLowerCase());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <E> E entity(Class<E> clazz, Map<String, ?> properties) {
+		U.notNull(clazz, "entity class");
+		Class<E> entityType = getEntityTypeFor(clazz);
+		U.notNull(entityType, "entity type");
+		if (entityType.isInterface() && IEntity.class.isAssignableFrom(entityType)) {
+			Class<? extends IEntity> cls = (Class<? extends IEntity>) entityType;
+			return (E) DbProxy.create(cls, properties);
+		} else {
+			E entity = Cls.newInstance(entityType);
+			Beany.update(entity, (Map<String, Object>) properties, true);
+			return entity;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <E> Class<E> getEntityTypeFor(Class<E> clazz) {
+		if (IEntity.class.isAssignableFrom(clazz)) {
+			if (Proxy.class.isAssignableFrom(clazz)) {
+				for (Class<?> interf : clazz.getInterfaces()) {
+					if (IEntity.class.isAssignableFrom(interf)) {
+						return (Class<E>) interf;
+					}
+				}
+				throw U.rte("Cannot find entity type for: %s!", clazz);
+			}
+		}
+		return clazz;
+	}
+
+	@Override
+	public Object entity(String rql, Object... args) {
+
+		String[] parts = rql.split(" ");
+
+		String entityName = U.capitalized(parts[0]);
+		Class<?> entityType = getEntityType(entityName);
+		U.must(entityType != null, "Cannot find entity '%s'!", entityName);
+
+		Map<String, Object> properties = U.map();
+		if (parts.length > 1) {
+
+			String[] props = rql.substring(entityName.length() + 1).split("\\s*\\,\\s*");
+
+			int argIndex = 0;
+			for (String prop : props) {
+				String[] kv = prop.trim().split("\\s*=\\s*");
+				String key = kv[0];
+				Object value;
+
+				if (kv.length > 1) {
+					value = kv[1].equals("?") ? args[argIndex++] : kv[1];
+				} else {
+					value = true;
+				}
+
+				properties.put(key, value);
+			}
+		}
+
+		return entity(entityType, properties);
+	}
+
+	@Override
+	public String toString() {
+		return "DbSchemaImpl [entityTypes=" + entityTypes + ", entityTypesPlural=" + entityTypesPlural + "]";
+	}
+
+}
