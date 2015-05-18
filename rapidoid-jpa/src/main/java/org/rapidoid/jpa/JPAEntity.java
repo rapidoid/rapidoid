f921945d40a77d24b1b179bd17b3e227254c07b0
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-18 13:59:18 +0200

    Introduced abstract JPA entity super-class.

---
+++ rapidoid-jpa/src/main/java/org/rapidoid/jpa/JPAEntity.java
@@ -0,0 +1,42 @@
+package org.rapidoid.jpa;
+
+/*
+ * #%L
+ * rapidoid-jpa
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class JPAEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
