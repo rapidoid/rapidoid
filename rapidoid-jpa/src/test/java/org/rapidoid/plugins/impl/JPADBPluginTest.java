e793b80408a759be0e9cb2b7c735cce83d1a661d
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-18 14:01:36 +0200

    Implemented test for the JPA-based DB plugin implementation.

---
+++ rapidoid-jpa/src/test/java/org/rapidoid/plugins/impl/JPADBPluginTest.java
@@ -0,0 +1,69 @@
+package org.rapidoid.plugins.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.entity.Book;
+import org.rapidoid.entity.Movie;
+import org.rapidoid.jpa.dbplugin.JPADBPlugin;
+import org.rapidoid.test.TestCommons;
+import org.testng.annotations.Test;
+
+/*
+ * #%L
+ * rapidoid-jpa
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public class JPADBPluginTest extends TestCommons {
+
+	@Test
+	public void testBasicCRUD() {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test-pu");
+		EntityManager em = emf.createEntityManager();
+
+		final JPADBPlugin db = new JPADBPlugin(em);
+
+		final Book b1 = new Book("book 1");
+		final Book b2 = new Book("book 2");
+		final Movie m1 = new Movie("movie 1");
+
+		db.transaction(new Runnable() {
+			@Override
+			public void run() {
+				db.insert(b1);
+				db.insert(b2);
+				db.insert(m1);
+			}
+		}, false);
+
+		List<Object> all = db.getAll();
+		eq(all.size(), 3);
+
+		em.close();
+		emf.close();
+	}
+
+}
