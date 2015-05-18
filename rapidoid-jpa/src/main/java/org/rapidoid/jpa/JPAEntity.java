b218fd5606b6065afc5b74e29d2e5359bc70550f
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-18 14:32:37 +0200

    Added versioning and identity support to the base JPAEntity class.

--- rapidoid-jpa/src/main/java/org/rapidoid/jpa/JPAEntity.java
+++ rapidoid-jpa/src/main/java/org/rapidoid/jpa/JPAEntity.java
@@ -23,7 +23,13 @@ package org.rapidoid.jpa;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
 @MappedSuperclass
 public abstract class JPAEntity {
 
@@ -31,12 +37,54 @@ public abstract class JPAEntity {
 	@GeneratedValue
 	private Long id;
 
+	@Version
+	private Long version;
+
 	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		JPAEntity other = (JPAEntity) obj;
+
+		if (id == null || other.id == null) {
+			return this == obj;
+		}
+
+		return id.equals(other.id);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "[id=" + id + ", version=" + version + "]";
 	}
 
 }
