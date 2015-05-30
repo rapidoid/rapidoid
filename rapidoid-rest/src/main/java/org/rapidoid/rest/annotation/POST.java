a23cb11a86a23249399b99f4190775aa8dd79521
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-30 20:09:28 +0200

    Introducing REST annotations.

---
+++ rapidoid-rest/src/main/java/org/rapidoid/rest/annotation/POST.java
@@ -0,0 +1,40 @@
+package org.rapidoid.rest.annotation;
+
+/*
+ * #%L
+ * rapidoid-rest
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+@Target({ METHOD })
+@Retention(RUNTIME)
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public @interface POST {
+
+	String value() default "";
+
+}
