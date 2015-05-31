0ae1a0e5e7865b7d93d6ec1f356aa8509e5d7369
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-31 23:03:54 +0200

    Added basic annotation-based AOP method invocation util.

---
+++ rapidoid-utils/src/main/java/org/rapidoid/aop/AOP.java
@@ -0,0 +1,58 @@
+package org.rapidoid.aop;
+
+/*
+ * #%L
+ * rapidoid-utils
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.util.Cls;
+import org.rapidoid.util.U;
+
+@Authors("Nikolche Mihajlovski")
+@Since("3.0.0")
+public class AOP {
+
+	private static Map<Class<? extends Annotation>, AOPInterceptor> INTERCEPTORS = U.synchronizedMap();
+
+	public static void register(Class<? extends Annotation> annotated, AOPInterceptor interceptor) {
+		INTERCEPTORS.put(annotated, interceptor);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T invoke(Object ctx, Method m, Object target, Object... args) {
+		// simple and limited AOP invocation interceptor, only 1 annotation pet method is supported
+
+		for (Annotation ann : m.getAnnotations()) {
+			Class<? extends Annotation> annotation = ann.annotationType();
+			AOPInterceptor interceptor = INTERCEPTORS.get(annotation);
+
+			if (interceptor != null) {
+				return (T) interceptor.intercept(ann, ctx, m, target, args);
+			}
+		}
+
+		return Cls.invoke(m, target, args);
+	}
+
+}
