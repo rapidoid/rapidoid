e253cd309caefa2fc3bfc584b58ec37958bdafed
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-07-25 12:10:26 +0200

    Introducing Rapidoid threads (normal threads with extras).

---
+++ rapidoid-activity/src/main/java/org/rapidoid/activity/RapidoidThread.java
@@ -0,0 +1,55 @@
+package org.rapidoid.activity;
+
+/*
+ * #%L
+ * rapidoid-activity
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.log.Log;
+import org.rapidoid.util.U;
+
+@Authors("Nikolche Mihajlovski")
+@Since("4.1.0")
+public abstract class RapidoidThread extends Thread {
+
+	private volatile long sleepMs = 5;
+
+	public RapidoidThread() {}
+
+	public RapidoidThread(long sleepMs) {
+		this.sleepMs = sleepMs;
+	}
+
+	@Override
+	public final void run() {
+		while (!Thread.interrupted()) {
+			try {
+				loop();
+			} catch (Throwable e) {
+				Log.error("Exception occured while watching for changes", e);
+			}
+
+			U.sleep(sleepMs);
+		}
+	}
+
+	protected abstract void loop();
+
+}
