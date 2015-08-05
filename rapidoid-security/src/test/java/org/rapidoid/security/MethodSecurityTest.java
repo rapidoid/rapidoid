da21a102284125755ff3dd4d8853325184147acb
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-08-05 18:25:05 +0200

    Added more method security tests.

--- rapidoid-security/src/test/java/org/rapidoid/security/MethodSecurityTest.java
+++ rapidoid-security/src/test/java/org/rapidoid/security/MethodSecurityTest.java
@@ -56,16 +56,51 @@ class MyService {
 
 }
 
+/*
+ * A ROLE MUST BE ALLOWED IN BOTH THE CLASS AND METHOD ANNOTATIONS
+ */
+@Manager
+@Roles({ @Role("root"), @Role("xyz") })
+class MyService2 {
+
+	@Admin
+	public void aa() {}
+
+	@Admin
+	@Moderator
+	@Manager
+	public void bb() {}
+
+	public void noAnn() {}
+
+	@Moderator
+	@Roles(@Role("abc"))
+	public void dd() {}
+
+	@Manager
+	@Roles({ @Role("abc"), @Role("xyz") })
+	public void ee() {}
+
+}
+
 @Authors("Nikolche Mihajlovski")
 @Since("4.1.0")
 public class MethodSecurityTest extends SecurityTestCommons {
 
+	private static final String ELSE = "asfdalkjsfasfd";
 	private Method aa;
 	private Method bb;
 	private Method noAnn;
 	private Method dd;
 	private Method ee;
 	private Method[] methods;
+
+	private Method aa2;
+	private Method bb2;
+	private Method noAnn2;
+	private Method dd2;
+	private Method ee2;
+	private Method[] methods2;
 
 	@Before
 	public void setup() {
@@ -76,6 +111,14 @@ public class MethodSecurityTest extends SecurityTestCommons {
 		ee = Cls.getMethod(MyService.class, "ee");
 
 		methods = new Method[] { aa, bb, noAnn, dd, ee };
+
+		aa2 = Cls.getMethod(MyService2.class, "aa");
+		bb2 = Cls.getMethod(MyService2.class, "bb");
+		noAnn2 = Cls.getMethod(MyService2.class, "noAnn");
+		dd2 = Cls.getMethod(MyService2.class, "dd");
+		ee2 = Cls.getMethod(MyService2.class, "ee");
+
+		methods2 = new Method[] { aa2, bb2, noAnn2, dd2, ee2 };
 
 		Conf.args("role-admin=adm1,adm2", "role-manager=mng1", "role-moderator=mod1,mod2", "role-abc=abc1,abc2",
 				"role-xyz=xyz1", "mode=production");
@@ -90,23 +133,51 @@ public class MethodSecurityTest extends SecurityTestCommons {
 		isFalse(Secure.canAccessMethod("mod2", aa));
 		isFalse(Secure.canAccessMethod("abc1", aa));
 		isFalse(Secure.canAccessMethod("xyz1", aa));
-		isFalse(Secure.canAccessMethod("asfdalkjsfasfd", aa));
+		isFalse(Secure.canAccessMethod(ELSE, aa));
 		isFalse(Secure.canAccessMethod("", aa));
 		isFalse(Secure.canAccessMethod(null, aa));
 	}
 
 	@Test
+	public void testAdminOnly2() {
+		isFalse(Secure.canAccessMethod("adm1", aa2));
+		isFalse(Secure.canAccessMethod("admin@debug", aa2));
+		isFalse(Secure.canAccessMethod("mng1", aa2));
+		isFalse(Secure.canAccessMethod("mod1", aa2));
+		isFalse(Secure.canAccessMethod("mod2", aa2));
+		isFalse(Secure.canAccessMethod("abc1", aa2));
+		isFalse(Secure.canAccessMethod("xyz1", aa2));
+		isFalse(Secure.canAccessMethod(ELSE, aa2));
+		isFalse(Secure.canAccessMethod("", aa2));
+		isFalse(Secure.canAccessMethod(null, aa2));
+	}
+
+	@Test
 	public void testSeveralSpecialRoles() {
 		isTrue(Secure.canAccessMethod("adm1", bb));
-		isFalse(Secure.canAccessMethod("admin@debug", aa));
+		isFalse(Secure.canAccessMethod("admin@debug", bb));
 		isTrue(Secure.canAccessMethod("mng1", bb));
 		isTrue(Secure.canAccessMethod("mod1", bb));
 		isTrue(Secure.canAccessMethod("mod2", bb));
 		isFalse(Secure.canAccessMethod("abc1", bb));
 		isFalse(Secure.canAccessMethod("xyz1", bb));
-		isFalse(Secure.canAccessMethod("asfdalkjsfasfd", bb));
+		isFalse(Secure.canAccessMethod(ELSE, bb));
 		isFalse(Secure.canAccessMethod("", bb));
 		isFalse(Secure.canAccessMethod(null, bb));
+	}
+
+	@Test
+	public void testSeveralSpecialRoles2() {
+		isFalse(Secure.canAccessMethod("adm1", bb2));
+		isFalse(Secure.canAccessMethod("admin@debug", aa2));
+		isTrue(Secure.canAccessMethod("mng1", bb2));
+		isFalse(Secure.canAccessMethod("mod1", bb2));
+		isFalse(Secure.canAccessMethod("mod2", bb2));
+		isFalse(Secure.canAccessMethod("abc1", bb2));
+		isFalse(Secure.canAccessMethod("xyz1", bb2));
+		isFalse(Secure.canAccessMethod(ELSE, bb2));
+		isFalse(Secure.canAccessMethod("", bb2));
+		isFalse(Secure.canAccessMethod(null, bb2));
 	}
 
 	@Test
@@ -117,9 +188,22 @@ public class MethodSecurityTest extends SecurityTestCommons {
 		isTrue(Secure.canAccessMethod("mod2", noAnn));
 		isTrue(Secure.canAccessMethod("abc1", noAnn));
 		isTrue(Secure.canAccessMethod("xyz1", noAnn));
-		isTrue(Secure.canAccessMethod("asfdalkjsfasfd", noAnn));
+		isTrue(Secure.canAccessMethod(ELSE, noAnn));
 		isTrue(Secure.canAccessMethod("", noAnn));
 		isTrue(Secure.canAccessMethod(null, noAnn));
+	}
+
+	@Test
+	public void testNoAnnotation2() {
+		isFalse(Secure.canAccessMethod("adm1", noAnn2));
+		isTrue(Secure.canAccessMethod("mng1", noAnn2));
+		isFalse(Secure.canAccessMethod("mod1", noAnn2));
+		isFalse(Secure.canAccessMethod("mod2", noAnn2));
+		isFalse(Secure.canAccessMethod("abc1", noAnn2));
+		isTrue(Secure.canAccessMethod("xyz1", noAnn2));
+		isFalse(Secure.canAccessMethod(ELSE, noAnn2));
+		isFalse(Secure.canAccessMethod("", noAnn2));
+		isFalse(Secure.canAccessMethod(null, noAnn2));
 	}
 
 	@Test
@@ -130,9 +214,22 @@ public class MethodSecurityTest extends SecurityTestCommons {
 		isTrue(Secure.canAccessMethod("mod2", dd));
 		isTrue(Secure.canAccessMethod("abc1", dd));
 		isFalse(Secure.canAccessMethod("xyz1", dd));
-		isFalse(Secure.canAccessMethod("asfdalkjsfasfd", dd));
+		isFalse(Secure.canAccessMethod(ELSE, dd));
 		isFalse(Secure.canAccessMethod("", dd));
 		isFalse(Secure.canAccessMethod(null, dd));
+	}
+
+	@Test
+	public void testCustomRole2() {
+		isFalse(Secure.canAccessMethod("adm1", dd2));
+		isFalse(Secure.canAccessMethod("mng1", dd2));
+		isFalse(Secure.canAccessMethod("mod1", dd2));
+		isFalse(Secure.canAccessMethod("mod2", dd2));
+		isFalse(Secure.canAccessMethod("abc1", dd2));
+		isFalse(Secure.canAccessMethod("xyz1", dd2));
+		isFalse(Secure.canAccessMethod(ELSE, dd2));
+		isFalse(Secure.canAccessMethod("", dd2));
+		isFalse(Secure.canAccessMethod(null, dd2));
 	}
 
 	@Test
@@ -143,14 +240,41 @@ public class MethodSecurityTest extends SecurityTestCommons {
 		isFalse(Secure.canAccessMethod("mod2", ee));
 		isTrue(Secure.canAccessMethod("abc1", ee));
 		isTrue(Secure.canAccessMethod("xyz1", ee));
-		isFalse(Secure.canAccessMethod("asfdalkjsfasfd", ee));
+		isFalse(Secure.canAccessMethod(ELSE, ee));
 		isFalse(Secure.canAccessMethod("", ee));
 		isFalse(Secure.canAccessMethod(null, ee));
 	}
 
 	@Test
+	public void testCustomRoles2() {
+		isFalse(Secure.canAccessMethod("adm1", ee2));
+		isTrue(Secure.canAccessMethod("mng1", ee2));
+		isFalse(Secure.canAccessMethod("mod1", ee2));
+		isFalse(Secure.canAccessMethod("mod2", ee2));
+		isFalse(Secure.canAccessMethod("abc1", ee2));
+		isTrue(Secure.canAccessMethod("xyz1", ee2));
+		isFalse(Secure.canAccessMethod(ELSE, ee2));
+		isFalse(Secure.canAccessMethod("", ee2));
+		isFalse(Secure.canAccessMethod(null, ee2));
+	}
+
+	@Test
 	public void testRoleAndUsernameMatch() {
 		for (Method method : methods) {
+			if (method != noAnn) {
+				isFalse(Secure.canAccessMethod("admin", method));
+				isFalse(Secure.canAccessMethod("manager", method));
+				isFalse(Secure.canAccessMethod("moderator", method));
+				isFalse(Secure.canAccessMethod("abc", method));
+				isFalse(Secure.canAccessMethod("xyz", method));
+				isFalse(Secure.canAccessMethod("loggedin", method));
+			}
+		}
+	}
+
+	@Test
+	public void testRoleAndUsernameMatch2() {
+		for (Method method : methods2) {
 			if (method != noAnn) {
 				isFalse(Secure.canAccessMethod("admin", method));
 				isFalse(Secure.canAccessMethod("manager", method));
