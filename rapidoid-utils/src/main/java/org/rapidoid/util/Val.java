139a557091fd2093f6bba7d1b74b5d033dbbe86c
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-09-26 20:39:13 +0200

    Introducing new helpers for convenience.

---
+++ rapidoid-utils/src/main/java/org/rapidoid/util/Val.java
@@ -0,0 +1,51 @@
+package org.rapidoid.util;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+/*
+ * #%L
+ * rapidoid-utils
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("4.2.0")
+public class Val<T> {
+
+	private volatile T value;
+
+	public Val() {}
+
+	public Val(T value) {
+		this.value = value;
+	}
+
+	public T get() {
+		return value;
+	}
+
+	public void set(T value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return U.readable(value);
+	}
+
+}
