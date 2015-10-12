9ca85dc67847e12911d3f9cd6d254c183ae32294
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-10-12 09:06:45 +0200

    Added simple XML util for convenient (de)serialization.

---
+++ rapidoid-jackson/src/main/java/org/rapidoid/xml/XML.java
@@ -0,0 +1,75 @@
+package org.rapidoid.xml;
+
+/*
+ * #%L
+ * rapidoid-jackson
+ * %%
+ * Copyright (C) 2014 - 2015 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.rapidoid.util.U;
+
+/**
+ * @author Nikolche Mihajlovski
+ * @since 4.4.0
+ */
+public class XML {
+
+	private XML() {}
+
+	public static String stringify(Object obj) {
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(obj.getClass());
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			jaxbMarshaller.marshal(obj, out);
+
+			return out.toString();
+
+		} catch (JAXBException e) {
+			throw U.rte(e);
+		}
+	}
+
+	public static <T> T parse(String xml, Class<T> valueType) {
+		return parse(xml.getBytes(), valueType);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T parse(byte[] xml, Class<T> valueType) {
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(valueType);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+			return (T) jaxbUnmarshaller.unmarshal(new ByteArrayInputStream(xml));
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
