d976371476f80e079589ab72ba94f37543897502
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-10-27 20:01:40 +0100

    Added highlight.js to the library pack and updated.

--- bin/assets.sh
+++ bin/assets.sh
@@ -7,7 +7,7 @@ cd assets
 cd ..
 
 # JS
-curl 'cdn.jsdelivr.net/g/underscorejs,jquery.cookie,jquery.easing,jquery.easy-pie-chart,jquery.validation(jquery.validate.min.js+additional-methods.min.js),jquery.parallax,jquery.prettycheckable,jquery.scrollto,jquery.timeago,angularjs(angular-sanitize.min.js+angular-resource.min.js+angular-animate.min.js+angular-cookies.min.js+angular-route.min.js+angular-loader.min.js+angular-touch.min.js),noty(packaged/jquery.noty.packaged.min.js),numeraljs,sortable,mustache.js,sweetalert,momentjs,select2,medium-editor,dropzone,typeahead.js,sortable' | sed 's/sourceMappingURL//g' > $DISTRO_JS
+curl 'cdn.jsdelivr.net/g/underscorejs,jquery.cookie,jquery.easing,jquery.easy-pie-chart,jquery.validation(jquery.validate.min.js+additional-methods.min.js),jquery.parallax,jquery.prettycheckable,jquery.scrollto,jquery.timeago,angularjs(angular-sanitize.min.js+angular-resource.min.js+angular-animate.min.js+angular-cookies.min.js+angular-route.min.js+angular-loader.min.js+angular-touch.min.js),noty(packaged/jquery.noty.packaged.min.js),numeraljs,sortable,mustache.js,sweetalert,momentjs,select2,medium-editor,dropzone,typeahead.js,sortable,highlight.js(highlight.min.js)' | sed 's/sourceMappingURL//g' > $DISTRO_JS
 cat assets/*.js >> $DISTRO_JS
 echo >> $DISTRO_JS
 echo >> $DISTRO_JS
@@ -17,7 +17,7 @@ echo >> $DISTRO_JS
 cat assets-ng/*.js >> $DISTRO_JS
 
 # CSS
-curl 'cdn.jsdelivr.net/g/sweetalert(sweetalert.css),select2(css/select2.min.css),medium-editor(css/medium-editor.min.css+css/themes/default.min.css),dropzone(dropzone.min.css)' > $DISTRO_CSS
+curl 'cdn.jsdelivr.net/g/sweetalert(sweetalert.css),select2(css/select2.min.css),medium-editor(css/medium-editor.min.css+css/themes/default.min.css),dropzone(dropzone.min.css),highlight.js(styles/default.min.css)' > $DISTRO_CSS
 cat assets/*.css >> $DISTRO_CSS
 cat assets-rapidoid/rapidoid-extras.css >> $DISTRO_CSS
 
