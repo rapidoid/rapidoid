0225b50b2a4b8012da9532309f6e10cc1ab6e115
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-02-14 18:57:17 +0100

    Renamed package.

--- rapidoid-web/src/main/java/org/rapidoid/web/AuthInterceptor.java
+++ rapidoid-web/src/main/java/org/rapidoid/web/AuthInterceptor.java
@@ -1,4 +1,4 @@
-package org.rapidoid.web.web;
+package org.rapidoid.web;
 
 /*
  * #%L
--- rapidoid-web/src/main/java/org/rapidoid/web/RapidoidWebModule.java
+++ rapidoid-web/src/main/java/org/rapidoid/web/RapidoidWebModule.java
@@ -1,4 +1,13 @@
-package org.rapidoid.web.web;
+package org.rapidoid.web;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.aop.AOP;
+import org.rapidoid.entity.AppClasspathEntitiesPlugin;
+import org.rapidoid.log.Log;
+import org.rapidoid.plugins.Plugins;
+import org.rapidoid.plugins.templates.MustacheTemplatesPlugin;
+import org.rapidoid.security.annotation.*;
 
 /*
  * #%L
@@ -9,9 +18,9 @@ package org.rapidoid.web.web;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,15 +28,6 @@ package org.rapidoid.web.web;
  * limitations under the License.
  * #L%
  */
-
-import org.rapidoid.annotation.Authors;
-import org.rapidoid.annotation.Since;
-import org.rapidoid.aop.AOP;
-import org.rapidoid.entity.AppClasspathEntitiesPlugin;
-import org.rapidoid.log.Log;
-import org.rapidoid.plugins.Plugins;
-import org.rapidoid.plugins.templates.MustacheTemplatesPlugin;
-import org.rapidoid.security.annotation.*;
 
 @Authors("Nikolche Mihajlovski")
 @Since("5.1.0")
@@ -41,8 +41,8 @@ public class RapidoidWebModule {
 
 		AOP.reset();
 
-		AOP.intercept(new AuthInterceptor(), Admin.class, Manager.class, Moderator.class, LoggedIn.class,
-				DevMode.class, Role.class, HasRole.class);
+		AOP.intercept(new AuthInterceptor(), Admin.class, Manager.class, Moderator.class, LoggedIn.class, DevMode.class,
+				Role.class, HasRole.class);
 	}
 
 }
