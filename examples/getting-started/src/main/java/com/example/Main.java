d4b5f8ec663094003bc18430727051f625264861
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-02-14 18:52:25 +0100

    Updated the Getting Started example.

--- examples/getting-started/src/main/java/com/example/Main.java
+++ examples/getting-started/src/main/java/com/example/Main.java
@@ -1,11 +1,12 @@
 package com.example;
 
-import org.rapidoid.http.fast.On;
+import org.rapidoid.http.On;
+import org.rapidoid.http.Req;
 
 public class Main {
 
 	public static void main(String[] args) {
-		On.get("/").json(req -> req.params());
+		On.get("/").json((Req req) -> req.params());
 		On.req(req -> req.uri());
 	}
 
