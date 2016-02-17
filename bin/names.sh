7ef8b849a568fdcee26041b3bd510ba6736fdf8a
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-02-17 10:39:03 +0100

    Temporary removed experimental plugins.

--- bin/names.sh
+++ bin/names.sh
@@ -1,6 +1,6 @@
 mvn clean
 
-for f in `grep -lrw --include=*.java -e "public interface" rapidoid-http rapidoid-http-api rapidoid-app rapidoid-plugins rapidoid-sql`; do
+for f in `grep -lrw --include=*.java -e "public interface" rapidoid-http rapidoid-http-api rapidoid-app rapidoid-sql`; do
    ls -l $f
    perl -pi -e 's/(\(\s*|,\s*)(?:\@P\([^)]+?\)\s+)?([^)]+?)\s+([a-z]\w+?\b)/\1\@P("\3") \2 \3/g' $f
    perl -pi -e 's/\@P\([^)]+?\)\s*(.*?)(\(\@P\(|\(\s*\))/\1\2/' $f
