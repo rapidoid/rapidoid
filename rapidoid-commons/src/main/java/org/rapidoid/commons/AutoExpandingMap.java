47df9b4bb52a2647a8010488f112e68d372db60c
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-03-08 11:12:07 +0100

    Improved the auto-expanding map performance, using double lock checking.

--- rapidoid-commons/src/main/java/org/rapidoid/commons/AutoExpandingMap.java
+++ rapidoid-commons/src/main/java/org/rapidoid/commons/AutoExpandingMap.java
@@ -1,5 +1,10 @@
 package org.rapidoid.commons;
 
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.lambda.Mapper;
+import org.rapidoid.u.U;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -13,9 +18,9 @@ import java.util.Set;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,11 +28,6 @@ import java.util.Set;
  * limitations under the License.
  * #L%
  */
-
-import org.rapidoid.annotation.Authors;
-import org.rapidoid.annotation.Since;
-import org.rapidoid.lambda.Mapper;
-import org.rapidoid.u.U;
 
 @Authors("Nikolche Mihajlovski")
 @Since("5.1.0")
@@ -44,21 +44,27 @@ public class AutoExpandingMap<K, V> implements Map<K, V> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public V get(Object key) {
-		synchronized (target) {
-			V val = target.get(key);
+		V val = target.get(key);
 
-			if (val == null) {
-				try {
-					val = valueFactory.map((K) key);
-				} catch (Exception e) {
-					throw U.rte(e);
+		if (val == null) {
+			synchronized (target) {
+				val = target.get(key);
+
+				if (val == null) {
+					try {
+						val = valueFactory.map((K) key);
+					} catch (Exception e) {
+						throw U.rte(e);
+					}
+
+					target.put((K) key, val);
 				}
 
-				target.put((K) key, val);
+				return val;
 			}
-
-			return val;
 		}
+
+		return val;
 	}
 
 	public AutoExpandingMap<K, V> copy() {
@@ -109,19 +115,14 @@ public class AutoExpandingMap<K, V> implements Map<K, V> {
 		if (getClass() != obj.getClass())
 			return false;
 		AutoExpandingMap<?, ?> other = (AutoExpandingMap<?, ?>) obj;
-		if (target == null) {
-			if (other.target != null)
-				return false;
-		} else if (!target.equals(other.target))
-			return false;
-		return true;
+		return target.equals(other.target);
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((target == null) ? 0 : target.hashCode());
+		result = prime * result + (target.hashCode());
 		return result;
 	}
 
