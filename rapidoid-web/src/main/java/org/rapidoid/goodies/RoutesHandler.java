c161bece42e083f69858545184262f1d3803f96b
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-05-26 00:13:45 +0200

    Minor improvement of the routes' roles info display.

--- rapidoid-web/src/main/java/org/rapidoid/goodies/RoutesHandler.java
+++ rapidoid-web/src/main/java/org/rapidoid/goodies/RoutesHandler.java
@@ -114,7 +114,7 @@ public class RoutesHandler extends GUI implements Callable<Object> {
 
 		Tag path = td(route.path());
 		Tag segment = td(config.segment());
-		Tag roles = td(config.roles());
+		Tag roles = td(display(config.roles().isEmpty() ? "": config.roles()));
 		Tag hnd = td(route.handler());
 
 		Tag ctype = td(config.contentType().info());
