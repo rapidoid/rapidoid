4029d25c984e199633a48cf152738e251db268ad
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-07-20 19:02:37 -0700

    Unified the location of JMustache templates and partials.

--- rapidoid-integrate/src/main/java/org/rapidoid/integrate/JMustacheViewRenderer.java
+++ rapidoid-integrate/src/main/java/org/rapidoid/integrate/JMustacheViewRenderer.java
@@ -23,9 +23,9 @@ import java.io.Reader;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -53,7 +53,7 @@ import java.io.Reader;
  *    renderer.setCompiler(Mustache.compiler().defaultValue("Template not found!"));
  * }</pre>
  */
-@Authors("kormakur")
+@Authors({"kormakur", "Nikolche Mihajlovski"})
 @Since("5.2.0")
 public class JMustacheViewRenderer extends RapidoidThing implements ViewRenderer {
 
@@ -61,7 +61,7 @@ public class JMustacheViewRenderer extends RapidoidThing implements ViewRenderer
 		@Override
 		public Reader getTemplate(String name) throws Exception {
 			String[] path = Customization.current().templatesPath();
-			return Res.from("partials/" + name + ".mustache", path).getReader();
+			return Res.from(name + ".html", path).getReader();
 		}
 	};
 
