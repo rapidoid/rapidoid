c3edcea0853c3f193c5327432828d126ff2233d7
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-08-26 15:23:22 -0700

    Introducing fast, non-blocking handlers.

---
+++ rapidoid-http-fast/src/main/java/org/rapidoid/http/handler/NonBlockingHttpHandler.java
@@ -0,0 +1,78 @@
+package org.rapidoid.http.handler;
+
+/*
+ * #%L
+ * rapidoid-http-fast
+ * %%
+ * Copyright (C) 2014 - 2016 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.http.FastHttp;
+import org.rapidoid.http.HttpStatus;
+import org.rapidoid.http.MediaType;
+import org.rapidoid.http.Req;
+import org.rapidoid.http.impl.HttpIO;
+import org.rapidoid.http.impl.RouteOptions;
+import org.rapidoid.net.abstracts.Channel;
+import org.rapidoid.util.Msc;
+
+import java.util.concurrent.Callable;
+
+@Authors("Nikolche Mihajlovski")
+@Since("5.2.0")
+public class NonBlockingHttpHandler extends AbstractHttpHandler {
+
+	private final FastHttp http;
+	private final Callable<Object> nonBlockingLogic;
+	private final boolean returnsJson;
+
+	public NonBlockingHttpHandler(FastHttp http, RouteOptions options, Callable<Object> nonBlockingLogic) {
+		super(options);
+
+		this.http = http;
+		this.nonBlockingLogic = nonBlockingLogic;
+		this.returnsJson = options.contentType() == MediaType.JSON;
+	}
+
+	@Override
+	public HttpStatus handle(Channel ctx, boolean isKeepAlive, Req req, Object extra) {
+		Object result;
+
+		try {
+			result = nonBlockingLogic.call();
+
+		} catch (Exception e) {
+			HttpIO.write200(ctx, isKeepAlive, contentType, "Internal server error!".getBytes());
+			return HttpStatus.ERROR;
+		}
+
+		if (returnsJson) {
+			HttpIO.writeAsJson(ctx, 200, isKeepAlive, result);
+		} else {
+			HttpIO.write200(ctx, isKeepAlive, contentType, Msc.toBytes(result));
+		}
+
+		return HttpStatus.DONE;
+	}
+
+	@Override
+	public String toString() {
+		return contentTypeInfo("() -> (non-blocking logic)");
+	}
+
+}
