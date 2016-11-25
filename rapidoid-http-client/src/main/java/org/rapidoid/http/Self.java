f6d753beb5018335e23ef4192f64a9f5e24b530a
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-11-25 15:16:02 +0100

    Self-connecting HTTP utils.

---
+++ rapidoid-http-client/src/main/java/org/rapidoid/http/Self.java
@@ -0,0 +1,74 @@
+package org.rapidoid.http;
+
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.config.Conf;
+import org.rapidoid.u.U;
+
+/*
+ * #%L
+ * rapidoid-http-client
+ * %%
+ * Copyright (C) 2014 - 2016 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+@Authors("Nikolche Mihajlovski")
+@Since("5.3.0")
+public class Self {
+
+	public static String localUrl(String urlPath) {
+		U.must(urlPath.startsWith("/"), "Invalid URL path, it must start with '/'!");
+		int port = Conf.APP.entry("port").or(8888);
+		return "http://localhost:" + port + urlPath;
+	}
+
+	public static HttpReq req(HttpVerb verb, String urlPath) {
+		return HTTP.req().verb(verb).url(localUrl(urlPath));
+	}
+
+	public static HttpReq get(String urlPath) {
+		return req(HttpVerb.GET, urlPath);
+	}
+
+	public static HttpReq post(String urlPath) {
+		return req(HttpVerb.POST, urlPath);
+	}
+
+	public static HttpReq put(String urlPath) {
+		return req(HttpVerb.PUT, urlPath);
+	}
+
+	public static HttpReq delete(String urlPath) {
+		return req(HttpVerb.DELETE, urlPath);
+	}
+
+	public static HttpReq patch(String urlPath) {
+		return req(HttpVerb.PATCH, urlPath);
+	}
+
+	public static HttpReq options(String urlPath) {
+		return req(HttpVerb.OPTIONS, urlPath);
+	}
+
+	public static HttpReq head(String urlPath) {
+		return req(HttpVerb.HEAD, urlPath);
+	}
+
+	public static HttpReq trace(String urlPath) {
+		return req(HttpVerb.TRACE, urlPath);
+	}
+
+}
