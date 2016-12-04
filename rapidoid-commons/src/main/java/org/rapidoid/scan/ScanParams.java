a2e2a9b12c1e901a9b7ed1a8948aa4ed7764df0c
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-12-04 15:12:13 +0100

    Proper identity of the Scan parameters, used for caching.

--- rapidoid-commons/src/main/java/org/rapidoid/scan/ScanParams.java
+++ rapidoid-commons/src/main/java/org/rapidoid/scan/ScanParams.java
@@ -8,6 +8,7 @@ import org.rapidoid.log.Log;
 import org.rapidoid.u.U;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -105,4 +106,27 @@ public class ScanParams extends RapidoidThing {
 			}
 		}
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		ScanParams that = (ScanParams) o;
+
+		if (!Arrays.equals(packages, that.packages)) return false;
+		if (matching != null ? !matching.equals(that.matching) : that.matching != null) return false;
+		if (!Arrays.equals(annotated, that.annotated)) return false;
+		return classLoader != null ? classLoader.equals(that.classLoader) : that.classLoader == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Arrays.hashCode(packages);
+		result = 31 * result + (matching != null ? matching.hashCode() : 0);
+		result = 31 * result + Arrays.hashCode(annotated);
+		result = 31 * result + (classLoader != null ? classLoader.hashCode() : 0);
+		return result;
+	}
+
 }
