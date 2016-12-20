e9f2fe4e438e9c20a381bce6fb9092d74399b83a
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-12-20 23:07:42 +0100

    Added test for the built-in HTTP caching.

---
+++ rapidoid-integration-tests/src/test/java/org/rapidoid/http/HttpCachingTest.java
@@ -0,0 +1,58 @@
+package org.rapidoid.http;
+
+/*
+ * #%L
+ * rapidoid-integration-tests
+ * %%
+ * Copyright (C) 2014 - 2016 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.junit.Test;
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+import org.rapidoid.setup.On;
+import org.rapidoid.u.U;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Authors("Nikolche Mihajlovski")
+@Since("5.3.0")
+public class HttpCachingTest extends IsolatedIntegrationTest {
+
+	@Test
+	public void testHttpCaching() {
+
+		// without caching
+		AtomicInteger x = new AtomicInteger();
+		On.get("/x").plain(() -> x.incrementAndGet());
+
+		// with caching
+		AtomicInteger y = new AtomicInteger();
+		On.get("/y").cacheTTL(2000).plain(() -> y.incrementAndGet());
+
+		int next = 1;
+		for (int n = 1; n <= 3; n++) {
+
+			for (int i = 0; i < 10; i++) {
+				Self.get("/x").expect("" + next++);
+				Self.get("/y").expect("" + n);
+			}
+
+			U.sleep(2100);
+		}
+	}
+
+}
