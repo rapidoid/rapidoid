b08f178775f0273e1e9352490ca5506466011f06
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-01-30 13:21:58 +0100

    Pre-configured dev-mode bootstrap with matching app deployment config.

---
+++ rapidoid-platform/src/main/java/org/rapidoid/standalone/PlatformInDevMode.java
@@ -0,0 +1,43 @@
+package org.rapidoid.standalone;
+
+/*
+ * #%L
+ * rapidoid-platform
+ * %%
+ * Copyright (C) 2014 - 2017 Nikolche Mihajlovski and contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.rapidoid.RapidoidThing;
+import org.rapidoid.annotation.Authors;
+import org.rapidoid.annotation.Since;
+
+@Authors("Nikolche Mihajlovski")
+@Since("5.3.0")
+public class PlatformInDevMode extends RapidoidThing {
+
+	private static final String[] DEV_MODE_ARGS = {
+		"app.services=welcome",
+		"admin.services=center",
+		"users.admin.password=admin",
+		"secret=my-secret",
+		"app.cdn=false",
+	};
+
+	public static void main(String[] args) {
+		Main.main(DEV_MODE_ARGS);
+	}
+
+}
