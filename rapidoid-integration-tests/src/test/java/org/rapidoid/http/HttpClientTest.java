e9856a4c7d19589e5f61adb10ed3df67b3d677c7
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-03-13 00:43:55 +0100

    Connecting on the correct port in the HTTP client test.

--- rapidoid-integration-tests/src/test/java/org/rapidoid/http/HttpClientTest.java
+++ rapidoid-integration-tests/src/test/java/org/rapidoid/http/HttpClientTest.java
@@ -55,7 +55,7 @@ public class HttpClientTest extends IsolatedIntegrationTest {
 
 			int count1 = 1000;
 			for (int i = 0; i < count1; i++) {
-				client.get("localhost", 8888, GET_LOCALHOST, hcb);
+				client.get("localhost", 8080, GET_LOCALHOST, hcb);
 			}
 
 			waiting();
