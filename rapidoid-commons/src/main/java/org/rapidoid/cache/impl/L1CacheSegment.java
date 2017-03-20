9b9acc4e7f8b19eb084066af8b52df3f125997d5
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-03-20 19:57:34 +0100

    Fixed NPE in the L1 cache.

--- rapidoid-commons/src/main/java/org/rapidoid/cache/impl/L1CacheSegment.java
+++ rapidoid-commons/src/main/java/org/rapidoid/cache/impl/L1CacheSegment.java
@@ -59,7 +59,7 @@ public class L1CacheSegment<K, V> extends RapidoidThing {
 		for (int i = 0; i < keys.length; i++) {
 			if (U.eq(keys[i], key)) {
 				ConcurrentCacheAtom<K, V> atom = atoms[i];
-				if (U.eq(atom.key, key)) {
+				if (atom != null && U.eq(atom.key, key)) {
 					return atom;
 				}
 			}
