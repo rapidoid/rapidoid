70a53fe272ff0da7ddfa9feed877118e21ce8a10
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-08-07 19:10:41 +0200

    Make sure the tests won't be adjusted during continuous integration.

--- rapidoid-test-commons/src/main/java/org/rapidoid/test/TestCommons.java
+++ rapidoid-test-commons/src/main/java/org/rapidoid/test/TestCommons.java
@@ -49,9 +49,10 @@ public abstract class TestCommons {
 
 	protected static final boolean RAPIDOID_CI = "true".equalsIgnoreCase(System.getenv("RAPIDOID_CI"));
 
-	private static final boolean ADJUST_TESTS = inDebugMode()
+	// don't adjust tests during continuous integration
+	private static final boolean ADJUST_TESTS = !RAPIDOID_CI && (inDebugMode()
 		|| "true".equalsIgnoreCase(System.getProperty("ADJUST_TESTS"))
-		|| "true".equalsIgnoreCase(System.getenv("ADJUST_TESTS"));
+		|| "true".equalsIgnoreCase(System.getenv("ADJUST_TESTS")));
 
 	protected static final Random RND = new Random();
 
