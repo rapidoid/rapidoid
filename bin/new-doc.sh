eebcddc211e08c79d16e87d434eb145e1ea934a6
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-08-12 19:26:39 +0200

    Adding new examples to the docs.

--- bin/new-doc.sh
+++ bin/new-doc.sh
@@ -7,6 +7,20 @@ CLS="${2}Test"
 
 echo "Adding new doc/example '$PKG', class $CLS"
 
+# ASCIIDOC
+
+cd asciidoc
+
+cp blank.adoc "${PKG}.adoc"
+
+sed -Ei "s/(blank|Blank)/$PKG/g" "${PKG}.adoc"
+
+printf '\n***\n\ninclude::'${PKG}'.adoc[]\n' >> topics.adoc
+
+cd ..
+
+# JAVA TESTS
+
 cd rapidoid-integration-tests/src/test/java/org/rapidoid/docs/
 cp -R blank $PKG
 
