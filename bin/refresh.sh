0e057f7e2651a87bff20755efebebda5e111c416
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-08-14 14:23:59 +0200

    Improved script: auto-commit the list of classes.

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 bin/pkgs.sh
 bin/license-update.sh
+
+git reset
+git add rapidoid-commons/src/main/resources/rapidoid-classes.txt
+git commit -m "Updated list of classes."
+git log -n 1
