921fbd342ad84d1c2af5d05ce965aec99764c3f8
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2017-10-19 15:11:42 +0200

    Adjusted paths of the commons due to restructuring.

--- bin/pkgs-list.sh
+++ bin/pkgs-list.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 for f in `grep -lrw --include=*.java . | grep -v "src/test"`; do
-   echo $f | perl -p -e 's/^(?:rapidoid|app-maven-plugin).*src\/main\/java\///g' | perl -p -e 's/.java//g' | perl -p -e 's/\//./g'
+   echo $f | perl -p -e 's/^(?:rapidoid|app-maven-plugin|commons).*src\/main\/java\///g' | perl -p -e 's/.java//g' | perl -p -e 's/\//./g'
 done
