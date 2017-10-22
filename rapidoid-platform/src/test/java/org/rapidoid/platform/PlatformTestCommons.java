cba3f4f06b170858a64c692180ba53ac58ee2c4f
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2017-10-22 17:24:34 +0200

    Introducing platform test commons.

--- rapidoid-platform/src/test/java/org/rapidoid/platform/PlatformTestCommons.java
+++ rapidoid-platform/src/test/java/org/rapidoid/platform/PlatformTestCommons.java
@@ -20,21 +20,18 @@ package org.rapidoid.platform;
  * #L%
  */
 
-import org.junit.Test;
 import org.rapidoid.annotation.Authors;
 import org.rapidoid.annotation.Since;
-import org.rapidoid.deploy.AppDownloader;
+import org.rapidoid.test.TestCommons;
+
+import java.io.File;
 
 @Authors("Nikolche Mihajlovski")
-@Since("5.3.0")
-public class AppDownloaderTest extends PlatformTestCommons {
+@Since("5.4.6")
+public class PlatformTestCommons extends TestCommons {
 
-	@Test
-	public void testAppUrlConstruction() {
-		eq(AppDownloader.getAppUrl("abc"), "https://github.com/rapidoid/abc/archive/master.zip");
-		eq(AppDownloader.getAppUrl("foo/bar"), "https://github.com/foo/bar/archive/master.zip");
-		eq(AppDownloader.getAppUrl("https://a.b.c.d.e"), "https://a.b.c.d.e");
-		eq(AppDownloader.getAppUrl("a/"), "a/");
+	static String appPath(String name) {
+		return new File("rapidoid-platform/src/test/" + name).getAbsolutePath();
 	}
 
 }
