51df6a99925db28684bc1433a75eb3d21680183a
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2018-01-25 15:25:22 +0100

    Reformatted code.

--- rapidoid-jpa/src/test/java/org/rapidoid/jpa/impl/CustomHibernatePersistenceProviderTest.java
+++ rapidoid-jpa/src/test/java/org/rapidoid/jpa/impl/CustomHibernatePersistenceProviderTest.java
@@ -6,17 +6,22 @@ import org.rapidoid.jdbc.JDBC;
 import org.rapidoid.jdbc.JdbcClient;
 import org.rapidoid.u.U;
 
+import javax.sql.DataSource;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 
 public class CustomHibernatePersistenceProviderTest {
-    @Test
-    public void testAlternativeHibernateConfigFile() {
-        JdbcClient h2 = JDBC.h2("test").init();
-        CustomHibernatePersistenceProvider provider = new CustomHibernatePersistenceProvider(h2.bootstrapDatasource());
-        Map props = U.map("hibernate.ejb.cfgfile", "hibernate.xml");
-        EntityManagerFactoryBuilder emfBuilder = provider.getEntityManagerFactoryBuilderOrNull("test", props, this.getClass().getClassLoader());
-        assertNotNull(emfBuilder);
-    }
+
+	@Test
+	public void testAlternativeHibernateConfigFile() {
+		JdbcClient h2 = JDBC.h2("test").init();
+		DataSource dataSource = h2.bootstrapDatasource();
+		CustomHibernatePersistenceProvider provider = new CustomHibernatePersistenceProvider(dataSource);
+
+		Map props = U.map("hibernate.ejb.cfgfile", "hibernate.xml");
+		EntityManagerFactoryBuilder emfBuilder = provider.getEntityManagerFactoryBuilderOrNull("test", props, this.getClass().getClassLoader());
+		assertNotNull(emfBuilder);
+	}
+
 }
