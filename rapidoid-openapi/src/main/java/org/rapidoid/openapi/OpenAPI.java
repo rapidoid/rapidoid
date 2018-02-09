782bfd927cb912b36e99ab119e421bee2b5d28af
danielbraga <daniel.souza.braga@gmail.com>
2018-02-09 19:12:32 -0200

    Create Open API Module #39 - Swagger integration Get paths and verbs

--- rapidoid-openapi/src/main/java/org/rapidoid/openapi/OpenAPI.java
+++ rapidoid-openapi/src/main/java/org/rapidoid/openapi/OpenAPI.java
@@ -24,8 +24,10 @@ package org.rapidoid.openapi;
 import org.rapidoid.RapidoidThing;
 import org.rapidoid.config.Conf;
 import org.rapidoid.config.Config;
+import org.rapidoid.http.HttpRoutes;
 import org.rapidoid.http.Req;
 import org.rapidoid.http.ReqHandler;
+import org.rapidoid.http.Route;
 import org.rapidoid.log.Log;
 import org.rapidoid.render.Template;
 import org.rapidoid.render.Templates;
@@ -34,9 +36,16 @@ import org.rapidoid.u.U;
 import org.rapidoid.util.Msc;
 import org.rapidoid.gui.GUI;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
 public class OpenAPI extends RapidoidThing {
 
     private static final Template OPENAPI_TEMPLATE = Templates.load("openapi-template.yaml");
+    private static final Template OPENAPI_PATH_TEMPLATE = Templates.load("openapi-path.yaml");
+    private static final Template OPENAPI_VERB_TEMPLATE = Templates.load("openapi-verb.yaml");
 
     private static final Config OPENAPI = Conf.OPENAPI;
 
@@ -47,7 +56,38 @@ public class OpenAPI extends RapidoidThing {
         }
 
         final StringBuilder openApiYaml = new StringBuilder();
-        openApiYaml.append(OPENAPI_TEMPLATE.render(U.map("openapi_project_version", "1.0", "openapi_project_title", "Project Title", "openapi_project_url", "http://api.vtracker.com.br")));
+        openApiYaml.append(OPENAPI_TEMPLATE.render(U.map("openapi_project_version", "1.0", "openapi_project_title", "Project Title", "openapi_project_url", "http://openapi.rapidoid.org")));
+
+        Hashtable<String, List<Route>> paths = null;
+        HttpRoutes routes = setup.routes();
+
+        if (routes != null && !routes.isEmpty()) {
+            Set<Route> routeSet = routes.all();
+            paths = new Hashtable<String, List<Route>>();
+
+            for (Route route: routeSet) {
+                String path = route.path().trim();
+                if (!paths.containsKey(path)) {
+                    paths.put(path, new ArrayList<Route>());
+                }
+
+                List<Route> routeList = paths.get(path);
+                routeList.add(route);
+            }
+        }
+
+        if (paths != null && paths.size() > 0) {
+            Set<String> keys = paths.keySet();
+            for (String path: keys) {
+                openApiYaml.append(OPENAPI_PATH_TEMPLATE.render(U.map("route_path", path)));
+
+                List<Route> routeListPath = paths.get(path);
+                for (Route route: routeListPath) {
+                    String verb = route.verb().name().toLowerCase();
+                    openApiYaml.append(OPENAPI_VERB_TEMPLATE.render(U.map("route_verb", verb)));
+                }
+            }
+        }
 
         final String openApiYamlPage = openApiYaml.toString();
 
--- rapidoid-openapi/src/test/java/org/rapidoid/openapi/OpenAPIDemo.java
+++ rapidoid-openapi/src/test/java/org/rapidoid/openapi/OpenAPIDemo.java
@@ -7,8 +7,19 @@ public class OpenAPIDemo {
 
     public static void main(String[] args) {
         Setup setup = On.setup();
+
+        On.get("/test1/").plain(OpenAPIDemo.sayHello());
+        On.get("/test2/saida").plain(OpenAPIDemo.sayHello());
+        On.get("/test2/output").plain(OpenAPIDemo.sayHello());
+        On.post("/test2/output").plain(OpenAPIDemo.sayHello());
+        On.delete("/test2/output").plain(OpenAPIDemo.sayHello());
+
         OpenAPI.register(setup);
         setup.activate();
+    }
+
+    public static String sayHello() {
+        return "Hello";
     }
 
 }
