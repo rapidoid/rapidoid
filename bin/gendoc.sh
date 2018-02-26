726fffb6839d88f63690449a27d14d2de80295fd
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2018-02-26 10:08:36 +0100

    Generating the release notes from the docs.

--- bin/gendoc.sh
+++ bin/gendoc.sh
@@ -5,3 +5,5 @@ IFS=$'\n\t'
 mvn generate-resources
 
 cp target/generated-docs/index.html ../rapidoid.github.io/docs-6.0.html
+
+cp target/generated-docs/notes.html ../rapidoid.github.io/release-notes.html
