72cb2acedda62bd343044d1d9570f02a0c24af03
Rikard Pavelic <rikard@ngs.hr>
2018-06-09 05:39:13 +0200

    DSL-JSON example integration

---
+++ examples/dsl-json-integration/src/main/java/com/example/DslJsonConverter.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.dslplatform.json.DslJson;
+import com.dslplatform.json.runtime.Settings;
+import org.rapidoid.RapidoidThing;
+import org.rapidoid.http.Req;
+import org.rapidoid.http.customize.BeanParameterFactory;
+import org.rapidoid.http.customize.HttpRequestBodyParser;
+import org.rapidoid.http.customize.HttpResponseRenderer;
+
+import java.io.OutputStream;
+import java.util.Map;
+
+public class DslJsonConverter extends RapidoidThing implements HttpResponseRenderer, HttpRequestBodyParser, BeanParameterFactory {
+
+	private final DslJson<Object> dslJson = new DslJson<>(Settings.withRuntime().includeServiceLoader());
+
+	@Override
+	public void render(Req req, Object value, OutputStream out) throws Exception {
+		dslJson.serialize(value, out);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Map<String, ?> parseRequestBody(Req req, byte[] body) throws Exception {
+		return null;
+	}
+
+	@Override
+	public Object getParamValue(Req req, Class<?> paramType, String paramName, Map<String, Object> properties) throws Exception {
+		byte[] body = req.body();
+		return dslJson.deserialize(paramType, body, body.length);
+	}
+}
--- examples/dsl-json-integration/src/main/java/com/example/DslJsonExample.java
+++ examples/dsl-json-integration/src/main/java/com/example/DslJsonExample.java
@@ -1,17 +1,31 @@
 package com.example;
 
+import com.dslplatform.json.CompiledJson;
 import org.rapidoid.setup.On;
 import org.rapidoid.u.U;
 
 public class DslJsonExample {
 
 	public static void main(String[] args) {
-		// a blank dsl-json integration example
+		DslJsonConverter dslJsonConverter = new DslJsonConverter();
+		On.custom().jsonResponseRenderer(dslJsonConverter);
+		On.custom().jsonRequestBodyParser(dslJsonConverter);
+		On.custom().beanParameterFactory(dslJsonConverter);
 
 		On.get("/hello").json(() -> U.map("msg", "Hello, world!"));
-
-		// TODO integrate the dsl-json library
+		On.post("/reflection").json((HelloReflection h) -> { h.x = h.x * 2; return h; });
+		On.post("/compiled").json((HelloCompiled h) -> { h.x = h.x * 2; return h; });
 	}
 
+	public static class HelloReflection {
+		public int x;
+		public String s;
+	}
+
+	@CompiledJson
+	public static class HelloCompiled {
+		public int x;
+		public String s;
+	}
 }
 
